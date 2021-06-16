3beb5982ed2d197fc2ea27a7b47b0ddf5ceac31b
awerebea <awerebea.21@gmail.com>
2021-06-16 23:10:03 +0300

    SCRIPTS Update rsync backup script

--- scripts/backup_home.sh
+++ scripts/backup_home.sh
@@ -11,7 +11,7 @@ SOURCE_PATH="/home/andrei"
 SNAPSHOT_PATH="/media/andrei/Data/backup"
 
 # Minimal timeout in minutes (6 hours)
-SNAPSHOT_TIMEOUT="21600"
+SNAPSHOT_TIMEOUT=$(( 60 * 60 * 6 ))
 
 SNAPSHOT_NAME=$(date +%Y-%m-%d_%H-%M-%S)
 # Just a split of too long line)
@@ -28,11 +28,69 @@ exit_err () {
 }
 
 create_snapshot () {
+  # Create differential snapshot
   rsync -aii --recursive --verbose --delete --force --stats --sparse \
     --log-file="${SNAPSHOT_PATH}/${SNAPSHOT_NAME}/${LOG_FILENAME}" \
     --include="/*/.*" --exclude="/.*" \
-    "${SOURCE_PATH}/" "${SNAPSHOT_PATH}/${SNAPSHOT_NAME}/${DATA_DIR_NAME}/"
+    "${SOURCE_PATH}/" "${SNAPSHOT_PATH}/${SNAPSHOT_NAME}/${DATA_DIR_NAME}/" ||
+    exit_err
+  touch ${SNAPSHOT_PATH}/${SNAPSHOT_NAME}
+  return
 }
+
+link_prev_snapshot () {
+  echo "Create links from previous snapshot: ${LAST_SNAPSHOT}"
+  echo "Please wait..."
+  rsync -a --exclude="/${LOG_FILENAME}" --link-dest="${SNAPSHOT_PATH}/${LAST_SNAPSHOT}" \
+    "${SNAPSHOT_PATH}/${LAST_SNAPSHOT}/" \
+    "${SNAPSHOT_PATH}/${SNAPSHOT_NAME}/" || exit_err
+  return
+}
+
+# Usage message
+usage () {
+  local MESSAGE=
+  read -r -d '' MESSAGE << __EOM__
+usage: ${0} [OPTIONS]
+Create differential backup of "${SOURCE_PATH}" directory into
+"${SNAPSHOT_PATH}" directory using rsync.
+Create snapshot only if last snapshot is older than TIMEOUT minutes.
+
+OPTIONS:
+  -f, --force         Ignore timeout
+  -t, --timeout NUM   Minimal timeout in minutes from last SNAPSHOT allowed
+                      (integer value, default 360)
+
+  -h, --help          print this help message and exit
+__EOM__
+  echo "${MESSAGE}"
+  return
+}
+
+# Command line options variables
+FORCE=
+
+# Process command line options
+while [[ -n "$1" ]]; do
+  case "$1" in
+  -f | --force)
+    FORCE="1"
+    ;;
+  -t | --timeout)
+    shift
+    SNAPSHOT_TIMEOUT=$(( "$1" * 60 ))
+    ;;
+  -h | --help)
+    usage
+    exit
+    ;;
+  *)
+    usage >&2
+    exit 1
+    ;;
+  esac
+  shift
+done
 
 # Get the newest directory (by name) with name a name matches the snapshot name
 # pattern
@@ -42,22 +100,22 @@ LAST_SNAPSHOT=$(ls -A -l -1 -r ${SNAPSHOT_PATH} | grep ^d | cut -d ' ' -f 9- |
 if [ -z ${LAST_SNAPSHOT} ]; then
   # Create first snapshot
   mkdir -p "${SNAPSHOT_PATH}/${SNAPSHOT_NAME}"
-  create_snapshot || exit_err
-  touch ${SNAPSHOT_PATH}/${SNAPSHOT_NAME}
+  create_snapshot
 else
-  # Get time of last snapshot and current time for comparsion
-  SNAPSHOT_TIME=$(stat --format='%Y' "${SNAPSHOT_PATH}/${LAST_SNAPSHOT}")
-  CURRENT_TIME=$(date +%s)
-  if (( SNAPSHOT_TIME < ( CURRENT_TIME - ( SNAPSHOT_TIMEOUT ) ) )); then
-    # Hardlink previous snapshot to the new one path
-    cp -al "${SNAPSHOT_PATH}/${LAST_SNAPSHOT}" \
-      "${SNAPSHOT_PATH}/${SNAPSHOT_NAME}" || exit_err
-    # Create differential snapshot
-    create_snapshot || exit_err
-    touch ${SNAPSHOT_PATH}/${SNAPSHOT_NAME}
+  if [[ -z ${FORCE} ]]; then
+    # Get time of last snapshot and current time for comparsion
+    SNAPSHOT_TIME=$(stat --format='%Y' "${SNAPSHOT_PATH}/${LAST_SNAPSHOT}")
+    CURRENT_TIME=$(date +%s)
+    if (( SNAPSHOT_TIME < ( CURRENT_TIME - SNAPSHOT_TIMEOUT ) )); then
+      link_prev_snapshot
+      create_snapshot
+    else
+      echo -e "Snapshot creation ${BROWN}skipped${NC} by timeout"
+      exit 0
+    fi
   else
-    echo -e "Snapshot creation ${BROWN}skipped${NC} by timeout"
-    exit 0
+      link_prev_snapshot
+      create_snapshot
   fi
 fi
 echo -e "Snapshot completed ${GREEN}successfully${NC}"
