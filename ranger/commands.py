131ad462c07390169f0a3a625cd92e89131687be
awerebea <awerebea.21@gmail.com>
2021-10-20 00:10:45 +0300

    RANGER Add fasd_dir command, change setting
    Don't save tabs on exit.

--- ranger/commands.py
+++ ranger/commands.py
@@ -203,6 +203,21 @@ class fasd(Command):
         return dirs
 
 
+class fasd_dir(Command):
+    def execute(self):
+        import subprocess
+        import os.path
+        fzf = self.fm.execute_command("fasd -dl | grep -iv cache | fzf 2>/dev/tty", universal_newlines=True, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.rstrip('\n'))
+            print(fzf_file)
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
+
+
 class paste_as_root(Command):
     def execute(self):
         if self.fm.do_cut:
