1fb4ecfbd84912384c254a9e4a278ee2c930516e
awerebea <awerebea.21@gmail.com>
2021-12-08 02:21:43 +0400

    SCRIPTS Add fingerprint_toggle.sh

---
+++ scripts/fingerprint_toggle.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+PS3=$'\n'"Choose an action: "
+options=(enable disable)
+select menu in "${options[@]}";
+do
+    if [[ $menu == "enable" ]]; then
+        sudo pam-auth-update --enable fprintd
+        break;
+    elif [[ $menu == "disable" ]]; then
+        sudo pam-auth-update --remove fprintd
+        break;
+    else
+        echo -e "\nYou picked ($REPLY), please select (1) to enable or (2) to disable"
+        continue
+    fi
+done
