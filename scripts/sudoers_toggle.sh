b9fe6d8f146bde1f3cc6aa12afbf62656f500282
awerebea <awerebea.21@gmail.com>
2021-12-08 02:18:11 +0400

    SCRIPTS Update sudoers_toggle.sh
    Add option to enable/disable fingerprint at the same time.

--- scripts/sudoers_toggle.sh
+++ scripts/sudoers_toggle.sh
@@ -1,12 +1,38 @@
 #!/usr/bin/env bash
 
+# Define colors
+YEL='\e[1;33m'
+END='\e[0m' # No Color
+
+function fingerprint_toggle() {
+    local ACTION
+    if [ "$1" == "disable" ]; then
+        ACTION="remove"
+    elif [ "$1" == "enable" ]; then
+        ACTION="enable"
+    fi
+    echo -en "${YEL}WRN:${END} Do you want to $1 fingerprint? (y|n): "
+    read -r ANS
+    case "${ANS}" in
+    [yY] | [yY][eE][sS])
+        sudo pam-auth-update --"$ACTION" fprintd
+        echo "Fingerprint ${ACTION}d"
+        ;;
+    *)
+        ;;
+    esac
+}
+
 if sudo grep -E "^# $USER ALL = NOPASSWD: ALL" /etc/sudoers > /dev/null; then
     sudo sed -i "s/^# $USER ALL = NOPASSWD: ALL/$USER ALL = NOPASSWD: ALL/" /etc/sudoers
     echo "sudo password protection DISABLED"
+    fingerprint_toggle disable
 elif sudo grep -E "^$USER ALL = NOPASSWD: ALL" /etc/sudoers > /dev/null; then
     sudo sed -i "s/^$USER ALL = NOPASSWD: ALL/# $USER ALL = NOPASSWD: ALL/" /etc/sudoers
     echo "sudo password protection ENABLED"
+    fingerprint_toggle enable
 else
     echo "$USER ALL = NOPASSWD: ALL" | sudo tee -a /etc/sudoers
     echo "sudo password protection DISABLED"
+    fingerprint_toggle disable
 fi
