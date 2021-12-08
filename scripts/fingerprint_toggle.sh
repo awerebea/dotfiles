ad76a56a02f5bae5c2f3134b9a3aef2b139290c6
awerebea <awerebea.21@gmail.com>
2021-12-08 12:30:08 +0400

    SCRIPTS Update fingerprint_toggle.sh
    Suppress error messages in pam-auth-update commands.

--- scripts/fingerprint_toggle.sh
+++ scripts/fingerprint_toggle.sh
@@ -5,10 +5,10 @@ options=(enable disable)
 select menu in "${options[@]}";
 do
     if [[ $menu == "enable" ]]; then
-        sudo pam-auth-update --enable fprintd
+        sudo pam-auth-update --enable fprintd 2> /dev/null
         break;
     elif [[ $menu == "disable" ]]; then
-        sudo pam-auth-update --remove fprintd
+        sudo pam-auth-update --remove fprintd 2> /dev/null
         break;
     else
         echo -e "\nYou picked ($REPLY), please select (1) to enable or (2) to disable"
