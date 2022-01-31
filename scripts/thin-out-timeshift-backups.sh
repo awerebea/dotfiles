ad88daae9d2954a43b1d05dd3443a8f1c07d0e9d
awerebea <awerebea.21@gmail.com>
2022-02-01 00:19:10 +0400

    SCRIPTS Add thin-out-timeshift-backups.sh
    Use in a cronjob to trim the Timeshift snapshots history to the limit
    passed as an argument.

---
+++ scripts/thin-out-timeshift-backups.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+[ $# -lt 1 ] && echo "Not enough arguments" >&2 && exit 1
+
+export SNAPSHOT_PATH="/media/andrei/bckp/timeshift"
+
+# Find all links in the snapshots-hourly dir, make file dates match their names.
+match-file-dates-with-file-names () {
+    find "$SNAPSHOT_PATH/snapshots-hourly" -mindepth 1 -maxdepth 1 -type l \
+        -print0 | xargs -0 -I '{}' sh -c 'sudo touch -h -t "$(basename {} |
+        sed "s/\(.*\)-/\1\./;s/-//g;s/_//")" {}'
+}
+match-file-dates-with-file-names
+
+# Find and delete all broken links after backups thin out
+remove-broken-links () {
+    find "$SNAPSHOT_PATH"  -mindepth 2 -maxdepth 2 -xtype l -print0 |
+        xargs -0 sudo rm 2>/dev/null || true
+}
+
+# Conditional remove dereferenced link passed as an argument
+remove-deref-link () {
+    [ $# -lt 1 ] && echo "Not enough arguments" >&2 && return 1
+    [ -z "$(jq -r ".comments" "$1/info.json")" ] &&
+        [ ! -L "$SNAPSHOT_PATH/snapshots-boot/$(basename "$1")" ] &&
+        [ ! -L "$SNAPSHOT_PATH/snapshots-daily/$(basename "$1")" ] &&
+        [ ! -L "$SNAPSHOT_PATH/snapshots-weekly/$(basename "$1")" ] &&
+        [ ! -L "$SNAPSHOT_PATH/snapshots-monthly/$(basename "$1")" ] &&
+        [ ! -L "$SNAPSHOT_PATH/snapshots-ondemand/$(basename "$1")" ] &&
+        echo -n "Removing snapshot: $(basename "$1") ... " &&
+        sudo rm -rf "$(readlink "$1")" && echo "successful!"
+}
+export -f remove-deref-link
+
+# Find and keep/remove every ${3}-th hourly snapshot newer than ${1} hours
+# but older than ${2} hours, if it's not commented and if it's not a boot,
+# daily, weekly, monthly or ondemand snapshot as well.
+# Keep or remove policy is defined by ${4}.
+thin-out-snapshots () {
+    [ $# -lt 4 ] && echo "Not enough arguments" >&2 && return 1
+    local OLD_PWD SED_ARGS
+    OLD_PWD="$PWD"
+    cd "$SNAPSHOT_PATH/snapshots-hourly" || return 1
+    if [ "$4" == "keep" ]; then
+        SED_ARGS=("1~$3d")
+    elif [ "$4" == "remove" ]; then
+        SED_ARGS=(-n "0~$3p")
+    fi
+    find . -mindepth 1 -maxdepth 1 -type l \
+        -mmin "-$(echo "$1 * 60" | bc)" -mmin "+$(echo "$2 * 60" | bc)" \
+        -printf '%h\0%d\0%p\n' | sort -t '\0' -n | awk -F'\0' '{print $3}' | \
+        sed ${SED_ARGS[*]} | xargs -I '{}' bash -c 'remove-deref-link "{}"' _
+    remove-broken-links
+    cd "$OLD_PWD" || return 1
+}
+
+# Check if snapshot history limit exceeded
+snapshot-history-limit-exceeded () {
+    [ $# -lt 1 ] && echo "Not enough arguments" >&2 && return 1
+    if [ "$(find "$SNAPSHOT_PATH/snapshots-hourly" -mindepth 1 -maxdepth 1 \
+        -type l -printf '%h\0%d\0%p\n' | sort -t '\0' -n | \
+        awk -F'\0' '{print $3}' | wc -l)" -gt "$1" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+snapshot-history-limit-exceeded "$1" && thin-out-snapshots 64 48 2 remove
+
+snapshot-history-limit-exceeded "$1" && thin-out-snapshots 48 40 2 remove
+
+snapshot-history-limit-exceeded "$1" && thin-out-snapshots 40 36 2 remove
+
+# Truncate hourly snapshot history to limit
+truncate-hourly-snapshots () {
+    [ $# -lt 1 ] && echo "Not enough arguments" >&2 && return 1
+    local SNAPSHOT
+    for SNAPSHOT in "$SNAPSHOT_PATH"/snapshots-hourly/*; do
+        if snapshot-history-limit-exceeded "$1"; then
+            remove-deref-link "$SNAPSHOT"
+            remove-broken-links
+        else
+            break
+        fi
+    done
+}
+
+truncate-hourly-snapshots "$1"
