0cab6ac46ca8cf69bdb0112a2c5ad1e759e325d1
awerebea <awerebea.21@gmail.com>
2022-06-14 12:34:44 +0400

    SCRIPTS Update scripts to hide/focus windows
    keepass2
    Qalculate!

--- scripts/keepass_mint.sh
+++ scripts/keepass_mint.sh
@@ -2,10 +2,10 @@
 
 if [ "$(wmctrl -xl | grep -c keepass2)" != 0 ]; then
     window_id=$(xdotool search --onlyvisible --classname keepass2)
-    if [ "$(xprop -id "$window_id" | grep -c '_NET_WM_STATE_HIDDEN')" == 0 ]; then
-        xdotool search --onlyvisible --classname --sync keepass2 windowminimize
-    else
+    if [ "$(xprop -id "$window_id" | grep -c '_NET_WM_STATE_FOCUSED')" == 0 ]; then
         wmctrl -a keepass
+    else
+        xdotool search --onlyvisible --classname --sync keepass2 windowminimize
     fi
 else
     keepass2
--- scripts/qalculate_mint.sh
+++ scripts/qalculate_mint.sh
@@ -2,10 +2,10 @@
 
 if [ "$(wmctrl -xl | grep -c qalculate-gtk)" != 0 ]; then
     window_id=$(xdotool search --onlyvisible --classname qalculate-gtk)
-    if [ "$(xprop -id "$window_id" | grep -c '_NET_WM_STATE_HIDDEN')" == 0 ]; then
-        xdotool search --onlyvisible --classname --sync qalculate-gtk windowminimize
-    else
+    if [ "$(xprop -id "$window_id" | grep -c '_NET_WM_STATE_FOCUSED')" == 0 ]; then
         wmctrl -a Qalculate!
+    else
+        xdotool search --onlyvisible --classname --sync qalculate-gtk windowminimize
     fi
 else
     qalculate-gtk
