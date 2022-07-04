b7278f0cbbc38f9568cbfc0612213e9f8e2eea0a
awerebea <awerebea.21@gmail.com>
2022-07-04 12:25:44 +0400

    SCRIPTS Add suppress-suspension.sh
    Script to suppress suspend if samba sharing or rclone sync is active.

---
+++ scripts/suppress-suspension.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+
+export DISPLAY=:0
+export XAUTHORITY=$HOME/.Xauthority
+
+ALLOW_IDLE=250000
+
+if [ "$(xprintidle)" -gt $ALLOW_IDLE ]; then
+    if [ "$(sudo smbstatus | grep --count DENY)" -gt 0 ] ||
+    [ "$(pgrep --count rclone)" -gt 0 ]; then
+        eval "$(xdotool getmouselocation --shell)"
+        xdotool mousemove "$X" "$Y"
+        exit 0
+    fi
+fi
