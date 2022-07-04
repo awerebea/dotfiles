c1df9205d71f224ce58ff3834b05e002332b8398
awerebea <awerebea.21@gmail.com>
2022-07-04 21:49:49 +0400

    SCRIPTS Update suppress-suspension.sh
    Reduce the ALLOW_IDLE timeout to 3 minutes to increase the cronjob
    interval to 12 minutes.

--- scripts/suppress-suspension.sh
+++ scripts/suppress-suspension.sh
@@ -3,7 +3,7 @@
 export DISPLAY=:0
 export XAUTHORITY=$HOME/.Xauthority
 
-ALLOW_IDLE=250000
+ALLOW_IDLE=180000
 
 if [ "$(xprintidle)" -gt $ALLOW_IDLE ]; then
     if [ "$(sudo smbstatus | grep --count DENY)" -gt 0 ] ||
