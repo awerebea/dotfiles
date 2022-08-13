a13c8d808554e7bd7e5c7df1c63e1510ed14593b
awerebea <awerebea.21@gmail.com>
2022-08-13 21:45:20 +0400

    SCRIPTS Reduce ALLOW_IDLE property

--- scripts/suppress-suspension.sh
+++ scripts/suppress-suspension.sh
@@ -3,7 +3,7 @@
 export DISPLAY=:0
 export XAUTHORITY=$HOME/.Xauthority
 
-ALLOW_IDLE=2650000
+ALLOW_IDLE=2600000
 
 if [ "$(xprintidle)" -gt $ALLOW_IDLE ]; then
     if [ "$(sudo smbstatus | grep --count DENY)" -gt 0 ] ||
