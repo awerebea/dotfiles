800b9c8e2f9ad6c1ad4fc39a06890cb262a96e3e
awerebea <awerebea.21@gmail.com>
2022-10-30 23:50:37 +0400

    SCRIPTS Update timeshift_custom.sh

--- scripts/timeshift_custom.sh
+++ scripts/timeshift_custom.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 
-# Run every 6 hours starting from boot time.
-[ "$(( $(awk '{print int($1/3600)}' /proc/uptime)%6 ))" -eq 0 ] && \
+# Run every N hours starting from boot time.
+N=3
+[ "$(( $(awk '{print int($1/3600)}' /proc/uptime)%N ))" -eq 0 ] && \
   timeshift --create --tags H --scripted
