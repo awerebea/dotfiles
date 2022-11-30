b7f06acd8991c0d51252bb031c2cac4a4aa4ce20
awerebea <awerebea.21@gmail.com>
2022-12-01 02:17:38 +0400

    NVIM Limit max string length to 99 for lua formatting

--- lua/plugins/lsp/null-ls.lua
+++ lua/plugins/lsp/null-ls.lua
@@ -18,7 +18,7 @@ null_ls.setup({
     --  to disable file types use
     --  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
     formatting.prettier, -- js/ts formatter
-    formatting.stylua, -- lua formatter
+    formatting.stylua.with({ extra_args = { "--column-width", "99" } }), -- lua formatter
     diagnostics.eslint_d.with({ -- js/ts linter
       -- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
       condition = function(utils)
