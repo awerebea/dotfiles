02f79224928c161542a6bc6a11487f5fdcf69c4a
awerebea <awerebea.21@gmail.com>
2022-12-04 14:57:45 +0400

    NVIM Add cmp-cmdline plugin

--- lua/plugins-setup.lua
+++ lua/plugins-setup.lua
@@ -87,6 +87,7 @@ return packer.startup(function(use)
   use("hrsh7th/nvim-cmp") -- completion plugin
   use("hrsh7th/cmp-buffer") -- source for text in buffer
   use("hrsh7th/cmp-path") -- source for file system paths
+  use("hrsh7th/cmp-cmdline") -- source for vim's cmdline
 
   -- snippets
   use("L3MON4D3/LuaSnip") -- snippet engine
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -42,6 +42,7 @@ cmp.setup({
     { name = "luasnip" }, -- snippets
     { name = "buffer" }, -- text within current buffer
     { name = "path" }, -- file system paths
+    { name = "cmdline" }, -- vim's cmdline
   }),
   -- configure lspkind for vs-code like icons
   formatting = {
