674f1862aac9f1a06b1e934ba862e49ad075f6d7
awerebea <awerebea.21@gmail.com>
2022-12-06 18:29:18 +0400

    Revert "NVIM Disable telescope exit with ESC in insert mode"
    This reverts commit 22883dbf9620d46f59ac2685272d853bae8e1727.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -22,7 +22,7 @@ telescope.setup({
         ["<C-k>"] = actions.move_selection_previous, -- move to prev result
         ["<C-j>"] = actions.move_selection_next, -- move to next result
         ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
-        --  ["<esc>"] = actions.close,
+        ["<esc>"] = actions.close,
       },
     },
   },
