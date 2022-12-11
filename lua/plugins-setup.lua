00a0887ac0677383487e5b7d6522e925ed46276d
awerebea <awerebea.21@gmail.com>
2022-12-11 21:23:37 +0400

    NVIM Add quick-scope plugin

--- lua/plugins-setup.lua
+++ lua/plugins-setup.lua
@@ -139,6 +139,7 @@ return packer.startup(function(use)
 
   -- Neovim motions on speed!
   use("phaazon/hop.nvim")
+  use("unblevable/quick-scope")
 
   -- a faster version of filetype.vim
   use("nathom/filetype.nvim")
