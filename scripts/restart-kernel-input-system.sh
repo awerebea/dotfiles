3ece14097f231193b3b6a8cb6840a9360b131a8d
awerebea <awerebea.21@gmail.com>
2022-12-27 17:16:16 +0400

    SCRIPTS Fix restart-kernel-input-system.sh
    Add a timeout between commands so that the mouse-middle-button-scroll
    script and the xmodmap command work correctly.

--- scripts/restart-kernel-input-system.sh
+++ scripts/restart-kernel-input-system.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 sudo udevadm trigger --subsystem-match=input --action=change
+sleep 2
 /home/andrei/.local/bin/mouse-middle-button-scroll
+sleep 2
 xmodmap -e 'keycode 118 = End'
