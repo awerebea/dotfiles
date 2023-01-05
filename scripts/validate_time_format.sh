c414430ba8ad8412c46e751c0c87686eeaa881bc
awerebea <awerebea.21@gmail.com>
2023-01-05 14:34:04 +0400

    SCRIPTS Fix time_delta and validate_time_format
    Fix "Value too great for base (error token is "08")" error.

--- scripts/validate_time_format.sh
+++ scripts/validate_time_format.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/env bash
 
 if [[ $1 =~ ^([0-9]{1,2}):([0-9]{1,2}):([0-9]{1,2})$ ]]; then
-    if (( BASH_REMATCH[3] < 60 )) && (( BASH_REMATCH[2] < 60 )) && (( BASH_REMATCH[1] < 24 )); then
+    if (( 10#${BASH_REMATCH[3]} < 60 )) && (( 10#${BASH_REMATCH[2]} < 60 )) &&
+    (( 10#${BASH_REMATCH[1]} < 24 )); then
         exit 0
     fi
 fi
