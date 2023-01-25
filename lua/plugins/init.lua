304998bf06082126c226d268b67e4e50cfcf5a5e
awerebea <awerebea.21@gmail.com>
2023-01-25 17:52:06 +0400

    NVIM Add auto-session plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -69,4 +69,22 @@ return {
     end,
   },
   { "christoomey/vim-tmux-navigator", event = { "VimEnter" } },
+  {
+    "rmagatti/auto-session",
+    event = { "VimEnter" },
+    config = function(_, _)
+      local opts = {
+        log_level = "info",
+        auto_session_enable_last_session = false,
+        auto_session_root_dir = vim.fn.stdpath "data" .. "/sessions/",
+        auto_session_enabled = true,
+        auto_save_enabled = nil,
+        auto_restore_enabled = nil,
+        auto_session_suppress_dirs = nil,
+        auto_session_use_git_branch = nil,
+        bypass_session_save_file_types = nil,
+      }
+      require("auto-session").setup(opts)
+    end,
+  },
 }
