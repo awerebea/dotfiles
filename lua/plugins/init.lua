a98e6d7d0e9dcdbd07cc5df7601e503710f4d233
awerebea <awerebea.21@gmail.com>
2023-01-27 00:29:29 +0400

    NVIM Add git-messenger plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -144,4 +144,13 @@ return {
     keys = { { "gz", "<Cmd>Bdelete<CR>" } },
   },
   { "nathom/filetype.nvim", event = "VimEnter" },
+  {
+    "rhysd/git-messenger.vim",
+    cmd = "GitMessenger",
+    keys = { "<leader>gm" },
+    config = function()
+      vim.g.git_messenger_always_into_popup = true
+      vim.g.git_messenger_floating_win_opts = { border = "single" }
+    end,
+  },
 }
