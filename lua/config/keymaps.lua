7569431c65320a9747d5f82edf5e6f20f3f0977f
awerebea <awerebea.21@gmail.com>
2023-01-26 22:26:40 +0400

    NVIM Refactor config keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -29,17 +29,15 @@ keymap("n", "<S-Down>", "<cmd>resize -2<CR>")
 keymap("n", "<S-Left>", "<cmd>vertical resize -2<CR>")
 keymap("n", "<S-Right>", "<cmd>vertical resize +2<CR>")
 
-local default_opts = { silent = true, noremap = true }
-
 ---------------------
 -- General Keymaps
 ---------------------
 
 -- Save file as sudo on files that require root permission
-keymap("c", "w!!", "execute 'silent! write !sudo tee % >/dev/null' <bar> edit!", default_opts)
+keymap("c", "w!!", "execute 'silent! write !sudo tee % >/dev/null' <bar> edit!", { silent = true })
 -- clear search highlights
-keymap("n", "<leader>/", "<Cmd>nohlsearch<CR>", default_opts)
-keymap("n", "<Esc><Esc>", "<Cmd>nohlsearch<CR>", default_opts)
+keymap("n", "<leader>/", "<Cmd>nohlsearch<CR>", { silent = true })
+keymap("n", "<Esc><Esc>", "<Cmd>nohlsearch<CR>", { silent = true })
 
 -- paste over without overwriting register
 vim.cmd [[
@@ -51,34 +49,34 @@ keymap({ "n", "v" }, "c", '"_c')
 keymap({ "n", "v" }, "C", '"_C')
 
 -- -- TODO: always move by screen lines instead of virtual lines with 'j' and 'k'
--- keymap("n", "j", "gj", default_opts)
--- keymap("n", "k", "gk", default_opts)
+-- keymap("n", "j", "gj", { silent = true })
+-- keymap("n", "k", "gk", { silent = true })
 
 -- window management
 keymap("n", "<leader>\\", "<C-w>v") -- split window vertically
 keymap("n", "<leader>-", "<C-w>s") -- split window horizontally
 keymap("n", "<leader>=", "<C-w>=") -- make split windows equal width & height
-keymap("n", "<leader>xy", "<Cmd>close<CR>", default_opts) -- close current split window
+keymap("n", "<leader>xy", "<Cmd>close<CR>", { silent = true }) -- close current split window
 -- buffers management
-keymap("n", "<M-S-l>", "<Cmd>bnext<CR>", default_opts)
-keymap("n", "<M-S-h>", "<Cmd>bprevious<CR>", default_opts)
-keymap("n", "gp", "<Cmd>bprevious<CR>", default_opts)
-keymap("n", "gn", "<Cmd>bnext<CR>", default_opts)
+keymap("n", "<M-S-l>", "<Cmd>bnext<CR>", { silent = true })
+keymap("n", "<M-S-h>", "<Cmd>bprevious<CR>", { silent = true })
+keymap("n", "gp", "<Cmd>bprevious<CR>", { silent = true })
+keymap("n", "gn", "<Cmd>bnext<CR>", { silent = true })
 -- tabs management
-keymap("n", "<leader>o", "<Cmd>tab ball<CR>", default_opts) -- open all buffers in tab
-keymap("n", "<leader><Left>", "<Cmd>tabprevious<CR>", default_opts)
-keymap("n", "<leader><Right>", "<Cmd>tabnext<CR>", default_opts)
-keymap("n", "<leader>tp", "<Cmd>tabprevious<CR>", default_opts)
-keymap("n", "<leader>tn", "<Cmd>tabnext<CR>", default_opts)
-keymap("n", "<leader>th", "<Cmd>tabfirst<CR>", default_opts)
-keymap("n", "<leader>tl", "<Cmd>tablast<CR>", default_opts)
-keymap("n", "<leader>te", "<Cmd>tabedit<Space>", default_opts)
-keymap("n", "<leader>tc", "<Cmd>tabnew<CR>") -- open new tab
-keymap("n", "<leader>tm", "<Cmd>tabmove<Space>", default_opts)
-keymap("n", "<leader>tmp", "<Cmd>-tabmove<CR>", default_opts) -- move current tab left
-keymap("n", "<leader>tmn", "<Cmd>+tabmove<CR>", default_opts) -- move current tab right
-keymap("n", "<leader>tx", "<Cmd>tabclose<CR>") -- close current tab
-keymap("n", "<leader>to", "<Cmd>tabonly<CR>", default_opts)
+keymap("n", "<leader>o", "<Cmd>tab ball<CR>", { silent = true }) -- open all buffers in tab
+keymap("n", "<leader><Left>", "<Cmd>tabprevious<CR>", { silent = true })
+keymap("n", "<leader><Right>", "<Cmd>tabnext<CR>", { silent = true })
+keymap("n", "<leader>tp", "<Cmd>tabprevious<CR>", { silent = true })
+keymap("n", "<leader>tn", "<Cmd>tabnext<CR>", { silent = true })
+keymap("n", "<leader>th", "<Cmd>tabfirst<CR>", { silent = true })
+keymap("n", "<leader>tl", "<Cmd>tablast<CR>", { silent = true })
+keymap("n", "<leader>te", "<Cmd>tabedit<Space>", { silent = true })
+keymap("n", "<leader>tc", "<Cmd>tabnew<CR>", { silent = true }) -- open new tab
+keymap("n", "<leader>tm", "<Cmd>tabmove<Space>", { silent = true })
+keymap("n", "<leader>tmp", "<Cmd>-tabmove<CR>", { silent = true }) -- move current tab left
+keymap("n", "<leader>tmn", "<Cmd>+tabmove<CR>", { silent = true }) -- move current tab right
+keymap("n", "<leader>tx", "<Cmd>tabclose<CR>", { silent = true }) -- close current tab
+keymap("n", "<leader>to", "<Cmd>tabonly<CR>", { silent = true })
 -- Switch to last tab
 vim.cmd [[
 if !exists('g:lasttab')
@@ -86,23 +84,28 @@ if !exists('g:lasttab')
 endif
 autocmd TabLeave * let g:lasttab = tabpagenr()
 ]]
-keymap("n", "<leader>tt", "<Cmd>exe 'tabn '.g:lasttab<CR>", default_opts) -- switch to last tab
-keymap("n", "gz", "<Cmd>bdelete<CR>", default_opts) -- close active buffer
+keymap("n", "<leader>tt", "<Cmd>exe 'tabn '.g:lasttab<CR>", { silent = true }) -- switch to last tab
+keymap("n", "gz", "<Cmd>bdelete<CR>", { silent = true }) -- close active buffer
 
 ----------------------
 -- Plugin Keybinds
 ----------------------
 
 -- restart lsp server (not on youtube nvim video)
-keymap("n", "<leader>rs", "<Cmd>LspRestart<CR>", default_opts) -- mapping to restart lsp if necessary
+keymap("n", "<leader>rs", "<Cmd>LspRestart<CR>", { silent = true }) -- mapping to restart lsp if necessary
 
 -- Toggle spell checking
-keymap("n", "<leader>ssa", "<Cmd>setlocal spell! spelllang=en_us,ru_yo<CR>", default_opts)
-keymap("n", "<leader>sse", "<Cmd>setlocal spell! spelllang=en_us<CR>", default_opts)
-keymap("n", "<leader>ssr", "<Cmd>setlocal spell! spelllang=ru_yo<CR>", default_opts)
+keymap("n", "<leader>ssa", "<Cmd>setlocal spell! spelllang=en_us,ru_yo<CR>", { silent = true })
+keymap("n", "<leader>sse", "<Cmd>setlocal spell! spelllang=en_us<CR>", { silent = true })
+keymap("n", "<leader>ssr", "<Cmd>setlocal spell! spelllang=ru_yo<CR>", { silent = true })
 
 -- Toggle show non-visible white spaces
-keymap("n", "<leader>ll", "<Cmd>set list!<CR>", default_opts)
+keymap("n", "<leader>ll", "<Cmd>set list!<CR>", { silent = true })
 
 -- Remove duplicate lines (leave only the last occurrence)
-keymap("n", "<leader>rd", "<Cmd>g/^\\(.*\\)\\ze\\n\\%(.*\\n\\)*\\1$/d<CR>", vim.tbl_extend("force", default_opts, { desc = "Remove duplicarte lines" }))
+keymap(
+  "n",
+  "<leader>rd",
+  "<Cmd>g/^\\(.*\\)\\ze\\n\\%(.*\\n\\)*\\1$/d<CR>",
+  { silent = true, desc = "Remove duplicarte lines" }
+)
