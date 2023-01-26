ff1771663254e98e3eb9261f97e42e52ab796925
awerebea <awerebea.21@gmail.com>
2023-01-26 20:45:23 +0400

    NVIM Update settings of gitsigns plugin

--- lua/plugins/vcs/init.lua
+++ lua/plugins/vcs/init.lua
@@ -26,31 +26,74 @@ return {
     event = "BufReadPre",
     opts = {
       signs = {
-        add = { hl = "GitSignsAdd", text = "▍", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+        add = {
+          hl = "GitSignsAdd",
+          text = "│",
+          numhl = "GitSignsAddNr",
+          linehl = "GitSignsAddLn",
+        },
         change = {
           hl = "GitSignsChange",
-          text = "▍",
+          text = "│",
           numhl = "GitSignsChangeNr",
           linehl = "GitSignsChangeLn",
         },
         delete = {
           hl = "GitSignsDelete",
-          text = "▸",
+          text = "_",
           numhl = "GitSignsDeleteNr",
           linehl = "GitSignsDeleteLn",
         },
         topdelete = {
           hl = "GitSignsDelete",
-          text = "▾",
+          text = "‾",
           numhl = "GitSignsDeleteNr",
           linehl = "GitSignsDeleteLn",
         },
         changedelete = {
           hl = "GitSignsChange",
-          text = "▍",
+          text = "~",
           numhl = "GitSignsChangeNr",
           linehl = "GitSignsChangeLn",
         },
+        untracked = {
+          hl = "GitSignsAdd",
+          text = "┆",
+          numhl = "GitSignsAddNr",
+          linehl = "GitSignsAddLn",
+        },
+      },
+      signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+      numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+      linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+      word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+      watch_gitdir = {
+        interval = 1000,
+        follow_files = true,
+      },
+      attach_to_untracked = true,
+      current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        delay = 1000,
+        ignore_whitespace = false,
+      },
+      current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+      sign_priority = 6,
+      update_debounce = 100,
+      status_formatter = nil, -- Use default
+      max_file_length = 40000, -- Disable if file is longer than this (in lines)
+      preview_config = {
+        -- Options passed to nvim_open_win
+        border = "single",
+        style = "minimal",
+        relative = "cursor",
+        row = 0,
+        col = 1,
+      },
+      yadm = {
+        enable = false,
       },
       -- update_debounce = 100,
       on_attach = function(bufnr)
@@ -61,6 +104,7 @@ return {
           vim.keymap.set(mode, l, r, opts)
         end
 
+        -- Navigation
         map("n", "]c", function()
           if vim.wo.diff then
             return "]c"
@@ -82,21 +126,21 @@ return {
         end, { expr = true })
 
         -- Actions
-        map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", { desc = "Stage Hunk" })
-        map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", { desc = "Reset Hunk" })
-        map("n", "<leader>ghS", gs.stage_buffer, { desc = "Stage Buffer" })
-        map("n", "<leader>ghu", gs.undo_stage_hunk, { desc = "Undo Stage Hunk" })
-        map("n", "<leader>ghR", gs.reset_buffer, { desc = "Reset Buffer" })
-        map("n", "<leader>ghp", gs.preview_hunk, { desc = "Preview Hunk" })
-        map("n", "<leader>ghb", function()
+        map({ "n", "v" }, "ghs", ":Gitsigns stage_hunk<CR>", { desc = "Stage Hunk" })
+        map({ "n", "v" }, "ghr", ":Gitsigns reset_hunk<CR>", { desc = "Reset Hunk" })
+        map("n", "ghS", gs.stage_buffer, { desc = "Stage Buffer" })
+        map("n", "ghu", gs.undo_stage_hunk, { desc = "Undo Stage Hunk" })
+        map("n", "ghR", gs.reset_buffer, { desc = "Reset Buffer" })
+        map("n", "ghp", gs.preview_hunk, { desc = "Preview Hunk" })
+        map("n", "ghb", function()
           gs.blame_line { full = true }
         end, { desc = "Blame Line" })
-        map("n", "<leader>gtb", gs.toggle_current_line_blame, { desc = "Toggle Line Blame" })
-        map("n", "<leader>ghd", gs.diffthis, { desc = "Diff This" })
-        map("n", "<leader>ghD", function()
+        map("n", "ghtb", gs.toggle_current_line_blame, { desc = "Toggle Line Blame" })
+        map("n", "ghd", gs.diffthis, { desc = "Diff This" })
+        map("n", "ghD", function()
           gs.diffthis "~"
         end, { desc = "Diff This ~" })
-        map("n", "<leader>gtd", gs.toggle_deleted, { desc = "Toggle Delete" })
+        map("n", "ghtd", gs.toggle_deleted, { desc = "Toggle Delete" })
 
         -- Text object
         map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "Select Hunk" })
