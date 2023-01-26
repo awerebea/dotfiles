a14faeb7ab74e56dd3d8102f081f1ed7f2b2dbb1
awerebea <awerebea.21@gmail.com>
2023-01-27 00:02:38 +0400

    NVIM Update settings of legendary plugin

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -2,8 +2,8 @@ return {
   {
     "mrjones2014/legendary.nvim",
     keys = {
-      { "<C-S-p>", "<cmd>Legendary<cr>", desc = "Legendary" },
-      { "<leader>hc", "<cmd>Legendary<cr>", desc = "Command Palette" },
+      { "<S-M-p>", "<Cmd>Legendary<CR>", desc = "Legendary" },
+      { "<leader>hc", "<Cmd>Legendary commands<CR>", desc = "Command Palette" },
     },
     opts = {
       which_key = { auto_register = true },
