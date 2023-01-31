d38ed1d611529fed00e4a7ae2dbcb5d878901381
awerebea <awerebea.21@gmail.com>
2023-01-31 21:07:24 +0400

    NVIM Disable comment continuation on a new line after a comment

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -8,3 +8,10 @@ vim.api.nvim_create_autocmd("User", {
     require "config.keymaps"
   end,
 })
+
+-- Disable new line comment (Placed here because otherwise it is overridden by /ftplugin/vim.vim)
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    vim.opt.formatoptions:remove { "c", "r", "o" }
+  end,
+})
