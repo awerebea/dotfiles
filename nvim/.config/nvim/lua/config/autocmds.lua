bf995a4d086b857fba2e0178caebbd9e8d1b575e
awerebea <awerebea.21@gmail.com>
2023-01-31 21:10:05 +0400

    NVIM Increase timeout of TextYankPost autocmd

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -2,7 +2,7 @@
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
-    vim.highlight.on_yank()
+    vim.highlight.on_yank { timeout = 250 }
   end,
   group = highlight_group,
   pattern = "*",
