b731157ef74de915263be540cb143311d85704af
awerebea <awerebea.21@gmail.com>
2023-02-10 18:04:15 +0400

    NVIM Do not follow current file in directory tree (nvim-tree)

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -27,7 +27,7 @@ return {
     sync_root_with_cwd = true,
     prefer_startup_root = true,
     update_focused_file = {
-      enable = true,
+      enable = false,
       update_root = true,
     },
     actions = {
