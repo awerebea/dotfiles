4b83399e737c5220fc4c17b8c690dce024ed0dd5
awerebea <awerebea.21@gmail.com>
2023-02-10 18:11:22 +0400

    NVIM Use <C-Space> to select and confirm the first suggestion

--- nvim/.config/nvim/lua/plugins/completion/init.lua
+++ nvim/.config/nvim/lua/plugins/completion/init.lua
@@ -38,7 +38,7 @@ return {
         mapping = cmp.mapping.preset.insert {
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-Space>"] = cmp.mapping.confirm { select = true },
           ["<C-e>"] = cmp.mapping { i = cmp.mapping.abort(), c = cmp.mapping.close() },
           ["<Esc><Esc>"] = cmp.mapping { i = cmp.mapping.abort(), c = cmp.mapping.close() },
           ["<CR>"] = cmp.mapping.confirm { select = false },
