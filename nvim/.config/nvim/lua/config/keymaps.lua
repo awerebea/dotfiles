c19f23a60d961220c97e50aaf3b28973a0238884
awerebea <awerebea.21@gmail.com>
2023-02-12 02:12:01 +0400

    NVIM Add keymaps to jump to closed folds

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -118,3 +118,33 @@ keymap(
 )
 
 keymap("n", "<leader>e", "<Cmd>edit<CR>")
+
+-- Jump to closed folds
+vim.cmd [[
+  function! NextClosedFold(dir)
+  let cmd = 'norm!z'..a:dir
+  let view = winsaveview()
+  let [l0, l, open] = [0, view.lnum, 1]
+  while l != l0 && open
+      exe cmd
+      let [l0, l] = [l, line('.')]
+      let open = foldclosed(l) < 0
+  endwhile
+  if open
+      call winrestview(view)
+  endif
+  endfunction
+
+  function! RepeatCmd(cmd) range abort
+      let n = v:count < 1 ? 1 : v:count
+      while n > 0
+          exe a:cmd
+          let n -= 1
+      endwhile
+  endfunction
+]]
+
+--stylua: ignore
+vim.keymap.set("n", "zj", ":<c-u>call RepeatCmd('call NextClosedFold(\"j\")')<cr>", { silent = true })
+--stylua: ignore
+vim.keymap.set("n", "zk", ":<c-u>call RepeatCmd('call NextClosedFold(\"k\")')<cr>", { silent = true })
