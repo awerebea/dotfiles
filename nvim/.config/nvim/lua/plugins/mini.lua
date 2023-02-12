1765824558a6ecc910b7e7babdea10ec8fa095e3
awerebea <awerebea.21@gmail.com>
2023-02-12 11:19:47 +0400

    NVIM Remove mini.map plugin as unused

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,16 +1,5 @@
 return {
   {
-    "echasnovski/mini.map",
-    opts = {},
-    keys = {
-      --stylua: ignore
-      { "<leader>vm", function() require("mini.map").toggle {} end, desc = "Toggle Minimap", },
-    },
-    config = function(_, opts)
-      require("mini.map").setup(opts)
-    end,
-  },
-  {
     "echasnovski/mini.jump",
     opts = {
       delay = {
