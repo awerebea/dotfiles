62e214f7e92e8dae3a8328de97535cdc3e756daa
awerebea <awerebea.21@gmail.com>
2023-02-13 22:26:17 +0400

    NVIM Add funcs and keymaps to find/grep ignored files with neoscope

--- nvim/.config/nvim/lua/plugins/neoscopes.lua
+++ nvim/.config/nvim/lua/plugins/neoscopes.lua
@@ -23,6 +23,27 @@ return {
     _G.neoscopes_find_files = function()
       require("telescope.builtin").find_files { search_dirs = scopes.get_current_dirs() }
     end
+    _G.neoscopes_find_ignored = function()
+      require("telescope.builtin").find_files {
+        search_dirs = scopes.get_current_dirs(),
+        find_command = {
+          "rg",
+          "--files",
+          "--hidden",
+          "-g",
+          "!.git",
+          "-g",
+          "!.venv",
+          "--no-ignore",
+        },
+      }
+    end
+    _G.neoscopes_grep_ignored = function()
+      require("telescope.builtin").live_grep {
+        search_dirs = scopes.get_current_dirs(),
+        additional_args = { "--no-ignore" },
+      }
+    end
     _G.neoscopes_live_grep = function()
       require("telescope.builtin").live_grep { search_dirs = scopes.get_current_dirs() }
     end
@@ -30,6 +51,10 @@ return {
     -- stylua: ignore
     vim.keymap.set( "n", "<leader>sf", ":lua neoscopes_find_files()<CR>", { desc = "Find files in scope" })
     -- stylua: ignore
+    vim.keymap.set( "n", "<leader>si", ":lua neoscopes_find_ignored()<CR>", { desc = "Find files in scope including ignored" })
+    -- stylua: ignore
     vim.keymap.set( "n", "<leader>s/", ":lua neoscopes_live_grep()<CR>", { desc = "Live grep in scope" })
+    -- stylua: ignore
+    vim.keymap.set( "n", "<leader>s?", ":lua neoscopes_grep_ignored()<CR>", { desc = "Live grep in scope including ignored" })
   end,
 }
