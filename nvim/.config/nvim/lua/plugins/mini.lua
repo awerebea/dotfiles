6e251874714d25b40be9e543ff30ca4630d35010
awerebea <awerebea.21@gmail.com>
2023-02-23 23:25:14 +0400

    NVIM Use VeryLazy event for loading mini.ai plugin

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -9,10 +9,7 @@ return {
   },
   {
     "echasnovski/mini.ai",
-    keys = {
-      { "a", mode = { "x", "o" } },
-      { "i", mode = { "x", "o" } },
-    },
+    event = "VeryLazy",
     dependencies = {
       {
         "nvim-treesitter/nvim-treesitter-textobjects",
