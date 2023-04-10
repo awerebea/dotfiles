b820d8c5e18f52cc4dfabaa01410fb24df1e6346
awerebea <awerebea.21@gmail.com>
2023-04-10 21:09:51 +0400

    NVIM Improve config/autocmds module

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,57 +1,68 @@
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
-vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank { timeout = 250 }
-  end,
-  group = highlight_group,
-  pattern = "*",
-})
-
--- Check if we need to reload the file when it changed
-vim.api.nvim_create_autocmd("FocusGained", { command = "checktime" })
-
--- go to last loc when opening a buffer
-vim.api.nvim_create_autocmd("BufReadPost", {
-  callback = function()
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    local lcount = vim.api.nvim_buf_line_count(0)
-    if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
-  end,
-})
-
--- windows to close
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = {
-    "OverseerForm",
-    "OverseerList",
-    "floggraph",
-    "fugitive",
-    "git",
-    "help",
-    "lspinfo",
-    "man",
-    "neotest-output",
-    "neotest-summary",
-    "qf",
-    "query",
-    "spectre_panel",
-    "startuptime",
-    "toggleterm",
-    "tsplayground",
-    "vim",
+local aucmd_dict = {
+  -- See `:help vim.highlight.on_yank()`
+  TextYankPost = {
+    {
+      callback = function()
+        vim.highlight.on_yank { timeout = 250 }
+      end,
+      group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
+      pattern = "*",
+    },
   },
-  callback = function(event)
-    vim.bo[event.buf].buflisted = false
-    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf })
-  end,
-})
+  -- Check if we need to reload the file when it changed
+  FocusGained = { { command = "checktime" } },
+  -- go to last loc when opening a buffer
+  BufReadPost = {
+    {
+      callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        local lcount = vim.api.nvim_buf_line_count(0)
+        if mark[1] > 0 and mark[1] <= lcount then
+          pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        end
+      end,
+    },
+  },
+  -- windows to close with q
+  FileType = {
+    {
+      pattern = {
+        "OverseerForm",
+        "OverseerList",
+        "floggraph",
+        "fugitive",
+        "git",
+        "help",
+        "lspinfo",
+        "man",
+        "neotest-output",
+        "neotest-summary",
+        "qf",
+        "query",
+        "spectre_panel",
+        "startuptime",
+        "toggleterm",
+        "tsplayground",
+        "vim",
+      },
+      callback = function(event)
+        vim.bo[event.buf].buflisted = false
+        vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf })
+      end,
+    },
+  },
+  -- don't auto comment new line
+  BufEnter = {
+    {
+      callback = function()
+        vim.opt.formatoptions:remove { "c", "r", "o" }
+      end,
+    },
+  },
+}
 
--- don't auto comment new line
-vim.api.nvim_create_autocmd("BufEnter", {
-  callback = function()
-    vim.opt.formatoptions:remove { "c", "r", "o" }
-  end,
-})
+for event, opt_tbls in pairs(aucmd_dict) do
+  for _, opt_tbl in pairs(opt_tbls) do
+    vim.api.nvim_create_autocmd(event, opt_tbl)
+  end
+end
