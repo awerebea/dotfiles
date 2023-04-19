3355bcf1ee97f87da7b25785420281db58f7ef9c
awerebea <awerebea.21@gmail.com>
2023-04-19 14:00:20 +0400

    TMUX, SCRIPTS Add tmux-cht.sh scripts to use https://cht.sh
    https://www.youtube.com/watch?v=hJzqEAf2U4I&ab_channel=ThePrimeagen
    https://github.com/ThePrimeagen/.dotfiles/blob/602019e902634188ab06ea31251c01c1a43d1621/bin/.local/scripts/tmux-cht.sh

---
+++ scripts/.local/bin/tmux-cht.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+selected=$(cat ~/.tmux/.tmux-cht-languages ~/.tmux/.tmux-cht-command | fzf)
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+read -rp "Enter Query: " query
+
+if grep -qs "$selected" ~/.tmux/.tmux-cht-languages; then
+    query=$(echo "$query" | tr ' ' '+')
+    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+else
+    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+fi
