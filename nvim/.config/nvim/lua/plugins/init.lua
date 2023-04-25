fbe31b46f8123c380d720dce36486b81bf62c33c
awerebea <awerebea.21@gmail.com>
2023-04-25 16:02:39 +0400

    NVIM Add muren plugin

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -315,4 +315,16 @@ return {
       vim.keymap.set("n", "<leader>/", ":Fz ", { desc = "Fuzzy slash" })
     end,
   },
+  {
+    "AckslD/muren.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>Mt", "<Cmd>MurenToggle<CR>" },
+      { "<leader>Mo", "<Cmd>MurenOpen<CR>" },
+      { "<leader>Mc", "<Cmd>MurenClose<CR>" },
+      { "<leader>Mf", "<Cmd>MurenFresh<CR>" },
+      { "<leader>Mu", "<Cmd>MurenUnique<CR>" },
+    },
+    config = true,
+  },
 }
