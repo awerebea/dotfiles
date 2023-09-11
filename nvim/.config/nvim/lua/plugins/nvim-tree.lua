8cdc29112d480d5c0f85dbc704dd1f3bfeb453ac
awerebea <awerebea.21@gmail.com>
2023-09-11 23:02:08 +0400

    NVIM Add default tmuxsend keymaps

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -85,7 +85,13 @@ return {
   },
   {
     "kiyoon/tmuxsend.vim",
-    event = "VeryLazy",
+    keys = {
+      { "-", "<Plug>(tmuxsend-smart)", mode = { "n", "x" } },
+      { "_", "<Plug>(tmuxsend-plain)", mode = { "n", "x" } },
+      { "<space>-", "<Plug>(tmuxsend-uid-smart)", mode = { "n", "x" } },
+      { "<space>_", "<Plug>(tmuxsend-uid-plain)", mode = { "n", "x" } },
+      { "<C-_>", "<Plug>(tmuxsend-tmuxbuffer)", mode = { "n", "x" } },
+    },
     config = function()
       vim.api.nvim_create_autocmd("FileType", {
         pattern = "NvimTree",
