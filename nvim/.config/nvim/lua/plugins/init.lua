9d7ef65735d8adec9eb4173dde85f2a2badccfc1
awerebea <awerebea.21@gmail.com>
2023-09-13 01:47:22 +0400

    NVIM Add hardtime plugin

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -336,4 +336,10 @@ return {
     },
     config = true,
   },
+  {
+    "m4xshen/hardtime.nvim",
+    event = "VeryLazy",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    opts = {},
+  },
 }
