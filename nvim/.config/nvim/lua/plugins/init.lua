681e615cc99cf8869ffbfe7513798387a87cd1b7
awerebea <awerebea.21@gmail.com>
2023-09-14 00:17:14 +0400

    NVIM Add nvim-devdocs plugin

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -363,4 +363,31 @@ return {
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     config = true,
   },
+  {
+    "luckasRanarison/nvim-devdocs",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {},
+    cmd = {
+      "DevdocsFetch",
+      "DevdocsInstall",
+      "DevdocsUninstall",
+      "DevdocsOpen",
+      "DevdocsOpenFloat",
+      "DevdocsOpenCurrent",
+      "DevdocsOpenCurrentFloat",
+      "DevdocsUpdate",
+      "DevdocsUpdateAll",
+    },
+    config = function()
+      require("nvim-devdocs").setup {
+        after_open = function(bufnr)
+          vim.api.nvim_buf_set_keymap(bufnr, "n", "<Esc>", "<Cmd>bdelete<CR>", {})
+        end,
+      }
+    end,
+  },
 }
