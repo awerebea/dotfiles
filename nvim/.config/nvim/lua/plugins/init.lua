6a11b249a8b123a428d5b90aea354f3b2583b5e4
awerebea <awerebea.21@gmail.com>
2023-09-15 14:13:14 +0400

    NVIM Pin window-picker version due to regression bug, update config

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -256,10 +256,23 @@ return {
   },
   {
     "s1n7ax/nvim-window-picker",
+    enabled = true,
+    version = "2.0.1",
     keys = { "<leader>w" },
     opts = {
       fg_color = "#000000",
       other_win_hl_color = "#1abc9c",
+      hint = "floating-big-letter",
+      show_prompt = false,
+      picker_config = { floating_big_letter = { font = "ansi-shadow" } },
+      filter_rules = {
+        autoselect_one = true,
+        include_current_win = false,
+        bo = {
+          filetype = { "NvimTree", "neo-tree", "notify", "incline", "fidget" },
+          buftype = { "terminal" },
+        },
+      },
     },
     config = function(_, opts)
       local picker = require "window-picker"
