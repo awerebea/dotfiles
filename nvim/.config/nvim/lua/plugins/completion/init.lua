11b3116e9f25360011f0faea75f8cdc5ecbcc8da
awerebea <awerebea.21@gmail.com>
2023-09-17 18:13:44 +0400

    NVIM Replace nvim-autopairs, tabout plugins with ultimate-autopair

--- nvim/.config/nvim/lua/plugins/completion/init.lua
+++ nvim/.config/nvim/lua/plugins/completion/init.lua
@@ -117,9 +117,6 @@ return {
           end,
         },
       }
-      -- Auto pairs
-      local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
     end,
   },
   {
