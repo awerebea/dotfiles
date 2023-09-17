258d12ecd5fd8cd3aaf7b1a06912ff7493d987a8
awerebea <awerebea.21@gmail.com>
2023-09-17 18:13:44 +0400

    NVIM Replace nvim-autopairs, tabout plugins with ultimate-autopair

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -485,4 +485,20 @@ return {
       vim.keymap.set({ "n" }, "<C-p>", require("pasta.mappings").toggle_pin)
     end,
   },
+  {
+    "altermo/ultimate-autopair.nvim",
+    enabled = true,
+    event = { "InsertEnter", "CmdlineEnter" },
+    branch = "v0.6",
+    opts = {
+      tabout = {
+        enable = true,
+        map = "<tab>",
+        cmap = "<S-tab>",
+        multi = false, --use multiple configs (|ultimate-autopair-map-multi-config|)
+        hopout = true, -- (|) > tabout > ()|
+        do_nothing_if_fail = false, --add a module so that if close fails then a `\t` will not be inserted
+      },
+    },
+  },
 }
