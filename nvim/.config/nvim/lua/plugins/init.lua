1147784ea70b7d6f6720f0d3702b37111cd04c31
awerebea <awerebea.21@gmail.com>
2023-09-20 14:21:34 +0400

    NVIM Disable fix indent in nvim-pasta config

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -475,7 +475,7 @@ return {
       require("pasta").setup {
         paste_mode = false,
         fix_cursor = true,
-        fix_indent = true,
+        fix_indent = false,
         prevent_diagnostics = false,
         next_key = vim.api.nvim_replace_termcodes("<C-n>", true, true, true),
         prev_key = vim.api.nvim_replace_termcodes("<C-p>", true, true, true),
