56ad039358f35def40bd06d7e0579e8a5341441a
awerebea <awerebea.21@gmail.com>
2023-09-26 11:11:41 +0400

    NVIM Update codeium plugin keymaps to match copilot ones

--- nvim/.config/nvim/lua/plugins/extras/ai/codeium.lua
+++ nvim/.config/nvim/lua/plugins/extras/ai/codeium.lua
@@ -6,11 +6,11 @@ return {
     -- stylua: ignore
     config = function ()
       vim.g.codeium_disable_bindings = 1
-      vim.keymap.set("i", "<C-l>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
-      vim.keymap.set("i", "<A-j>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
-      vim.keymap.set("i", "<A-k>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
-      vim.keymap.set("i", "<A-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
-      vim.keymap.set("i", "<A-s>", function() return vim.fn["codeium#Complete"]() end, { expr = true })
+      vim.keymap.set("i", "<M-l>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
+      vim.keymap.set("i", "<M-]>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
+      vim.keymap.set("i", "<M-[>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
+      vim.keymap.set("i", "<C-]>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
+      vim.keymap.set("i", "<M-a>", function() return vim.fn["codeium#Complete"]() end, { expr = true })
     end,
   },
 }
