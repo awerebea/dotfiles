5443fb717c35f449c9ea4d37de10d03af10b8c20
awerebea <awerebea.21@gmail.com>
2023-09-26 11:04:22 +0400

    NVIM Add borders to completion windows

--- nvim/.config/nvim/lua/plugins/completion/init.lua
+++ nvim/.config/nvim/lua/plugins/completion/init.lua
@@ -73,6 +73,10 @@ return {
             "c",
           }),
         },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
         sources = cmp.config.sources {
           { name = "nvim_lsp_signature_help" },
           { name = "nvim_lsp" },
