fc8bcdd8b7d5226c8b8ab5e2212feb2f68573787
awerebea <awerebea.21@gmail.com>
2023-10-24 12:35:52 +0400

    NVIM Update notify plugin options

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -40,13 +40,15 @@ return {
     "rcarriga/nvim-notify",
     event = "VeryLazy",
     opts = {
-      timeout = 3000,
+      timeout = 2500,
       max_height = function()
         return math.floor(vim.o.lines * 0.75)
       end,
       max_width = function()
         return math.floor(vim.o.columns * 0.75)
       end,
+      render = "wrapped-compact",
+      top_down = false,
     },
     config = function(_, opts)
       require("notify").setup(opts)
