4d31dcc70fcb60bf27e89e081d1033ade0e5c60f
awerebea <awerebea.21@gmail.com>
2023-11-13 00:52:13 +0400

    NVIM Switch to catppuccin theme

--- nvim/.config/nvim/lua/plugins/colorscheme/init.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme/init.lua
@@ -61,8 +61,51 @@ return {
     lazy = false,
     name = "catppuccin",
     config = function()
-      require("catppuccin").setup()
-      -- require("catppuccin").load()
+      local catppuccin = require "catppuccin"
+      catppuccin.setup {
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        dim_inactive = {
+          enabled = true, -- dims the background color of inactive window
+          shade = "dark",
+          percentage = 0.15, -- percentage of the shade to apply to the inactive window
+        },
+        styles = {
+          comments = { "italic" },
+          conditionals = { "italic" },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+        },
+        integrations = {
+          notify = true,
+          aerial = true,
+          harpoon = true,
+          which_key = true,
+        },
+      }
+      catppuccin.load()
+
+      -- {{{ Colors for words that failed spell check
+      -- Word not recognized
+      vim.api.nvim_set_hl(0, "SpellBad", {})
+      vim.api.nvim_set_hl(0, "SpellBad", { undercurl = true, fg = "#00bfff" })
+      -- Word not capitalized
+      vim.api.nvim_set_hl(0, "SpellCap", {})
+      vim.api.nvim_set_hl(0, "SpellCap", { undercurl = true, fg = "#ff4500" })
+      -- Word is rare
+      vim.api.nvim_set_hl(0, "SpellRare", {})
+      vim.api.nvim_set_hl(0, "SpellRare", { undercurl = true, fg = "#32cd32" })
+      -- Wrong spelling for selected region
+      vim.api.nvim_set_hl(0, "SpellLocal", {})
+      vim.api.nvim_set_hl(0, "SpellLocal", { undercurl = true, fg = "#ffb90f" })
+      -- }}}
     end,
   },
   {
