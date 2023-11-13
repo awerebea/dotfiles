e19691ab465dffb60929e50134bc886c0e842a85
awerebea <awerebea.21@gmail.com>
2023-11-13 23:45:06 +0400

    NVIM Add hbac.nvim plugin

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -529,4 +529,23 @@ return {
       { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
   },
+  {
+    "axkirillov/hbac.nvim",
+    event = "VeryLazy",
+    opts = {
+      autoclose = false, -- set autoclose to false if you want to close manually
+      telescope = {
+        -- mappings = {
+        --   i = {
+        --     ["<M-c>"] = actions.close_unpinned,
+        --     ["<M-x>"] = actions.delete_buffer,
+        --     ["<M-a>"] = actions.pin_all,
+        --     ["<M-u>"] = actions.unpin_all,
+        --     ["<M-y>"] = actions.toggle_pin,
+        --   },
+        -- },
+      },
+    },
+    config = true,
+  },
 }
