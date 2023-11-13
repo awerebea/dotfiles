d9f5ba87ff5a93d8d70b79b03fc599ff21f51daf
awerebea <awerebea.21@gmail.com>
2023-11-13 20:38:36 +0400

    NVIM Integrate flash.nvim with Telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -536,6 +536,34 @@ return {
           },
         },
       }
+
+      -- {{{ flash.nvim integration
+      local function flash(prompt_bufnr)
+        require("flash").jump {
+          pattern = "^",
+          label = { after = { 0, 0 } },
+          search = {
+            mode = "search",
+            exclude = {
+              function(win)
+                return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "TelescopeResults"
+              end,
+            },
+          },
+          action = function(match)
+            local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+            picker:set_selection(match.pos[1] - 1)
+          end,
+        }
+      end
+      opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
+        mappings = {
+          n = { s = flash },
+          i = { ["<c-s>"] = flash },
+        },
+      })
+      -- }}} flash.nvim integration
+
       telescope.setup(opts)
       telescope.load_extension "aerial"
       telescope.load_extension "fzf"
