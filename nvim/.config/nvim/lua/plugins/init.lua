06a80b3ad1f8c2ba65cf3e9af31af36e35358836
awerebea <awerebea.21@gmail.com>
2023-11-27 20:24:46 +0400

    NVIM Replace vim-maximizer plugin with windows.nvim

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -147,28 +147,33 @@ return {
     config = true,
   },
   {
-    "szw/vim-maximizer",
-    event = "VeryLazy",
-    config = function()
-      vim.keymap.set(
-        { "n", "v" },
-        "<leader>z",
-        "<Cmd>MaximizerToggle!<CR>",
-        { desc = "Maximize window toggle" }
-      )
-      vim.g.maximizer_set_default_mapping = 1
-      vim.g.maximizer_set_mapping_with_bang = 1
-      vim.g.maximizer_default_mapping_key = "<F3>"
-      vim.opt.winminheight = 0
-      vim.opt.winminwidth = 0
-    end,
-  },
-  {
     "simeji/winresizer",
     cmd = "WinResizerStartResize",
     keys = { "<C-e>" },
     config = function(_, _)
       vim.g.winresizer_finish_with_escape = 1
+    end,
+  },
+  {
+    "anuvyklack/windows.nvim",
+    event = "VeryLazy",
+    dependencies = { "anuvyklack/middleclass" },
+    opts = { autowidth = { winwidth = 12 } },
+    config = function(_, opts)
+      vim.opt.winminwidth = 5
+      vim.opt.winheight = 5
+      vim.opt.winminheight = 0
+      vim.opt.equalalways = false
+      require("windows").setup(opts)
+      local function cmd(command)
+        return table.concat { "<Cmd>", command, "<CR>" }
+      end
+      vim.keymap.set("n", "<C-w>z", cmd "WindowsMaximize")
+      vim.keymap.set("n", "<leader>z", cmd "WindowsMaximize")
+      vim.keymap.set("n", "<C-w>-", cmd "WindowsMaximizeVertically")
+      vim.keymap.set("n", "<C-w>\\", cmd "WindowsMaximizeHorizontally")
+      vim.keymap.set("n", "<C-w>=", cmd "WindowsEqualize")
+      vim.keymap.set("n", "<C-w>t", cmd "WindowsToggleAutowidth")
     end,
   },
   {
