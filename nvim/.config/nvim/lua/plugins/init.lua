5499775792403d49ef27e73f537620938bb934cb
awerebea <awerebea.21@gmail.com>
2023-11-28 09:37:04 +0400

    NVIM Set resize step explicitly, winresizer

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -152,6 +152,8 @@ return {
     keys = { "<C-e>" },
     config = function(_, _)
       vim.g.winresizer_finish_with_escape = 1
+      vim.g.winresizer_vert_resize = 3
+      vim.g.winresizer_horiz_resize = 3
     end,
   },
   {
