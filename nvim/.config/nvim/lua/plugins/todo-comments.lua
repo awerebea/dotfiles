06c955afb4fd2c83023560ca8e09a057d729416d
awerebea <awerebea.21@gmail.com>
2023-12-01 12:30:11 +0400

    NVIM Add new keymaps and update description, todo-comments

--- nvim/.config/nvim/lua/plugins/todo-comments.lua
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -4,10 +4,12 @@ return {
   cmd = { "TodoTrouble", "TodoTelescope" },
   -- stylua: ignore
   keys = {
-    { "<leader>]t", function() require("todo-comments").jump_next() end, desc = "Next ToDo" },
-    { "<leader>[t", function() require("todo-comments").jump_prev() end, desc = "Previous ToDo" },
-    { "<leader>ct", "<cmd>TodoTrouble<cr>", desc = "ToDo (Trouble)" },
-    { "<leader>cT", "<cmd>TodoTelescope<cr>", desc = "ToDo" },
+    { "<leader>]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
+    { "<leader>[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
+    { "<leader>cT", "<cmd>TodoTrouble<CR>", desc = "todo-comments (Trouble)" },
+    { "<leader>ct", "<cmd>TodoTelescope<CR>", desc = "todo-comments (Telescope)" },
+    { "<leader>cq", "<cmd>TodoQuickFix<CR>", desc = "todo-comments (quickfix)" },
+    { "<leader>cl", "<cmd>TodoLocList<CR>", desc = "todo-comments (loclist)" },
   },
   config = true,
 }
