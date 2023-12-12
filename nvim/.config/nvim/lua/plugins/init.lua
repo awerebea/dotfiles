ab79889a28634125aaede4630e3063b8fc2a19be
awerebea <awerebea.21@gmail.com>
2023-12-12 20:23:32 +0400

    NVIM Remove fuzzy_slash plugin

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -416,20 +416,6 @@ return {
     config = true,
   },
   {
-    "IndianBoy42/fuzzy_slash.nvim",
-    dependencies = {
-      {
-        "IndianBoy42/fuzzy.nvim",
-        dependencies = { { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
-      },
-    },
-    event = "VeryLazy",
-    config = function(opts)
-      require("fuzzy_slash").setup(opts)
-      vim.keymap.set("n", "<leader>/", ":Fz ", { desc = "Fuzzy slash" })
-    end,
-  },
-  {
     "AckslD/muren.nvim",
     event = "VeryLazy",
     keys = {
