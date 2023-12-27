dbd4bc3e9e3245e158fbfe8da88038d1b6603a40
awerebea <awerebea.21@gmail.com>
2023-12-27 12:42:57 +0400

    NVIM Add neowords plugin

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -653,4 +653,32 @@ return {
       imap("<M-,>", ft.repeat_backward, "Repeat hop backward to a last given char")
     end,
   },
+  {
+    "backdround/neowords.nvim",
+    event = "VeryLazy",
+    config = function()
+      local neowords = require "neowords"
+      local presets = neowords.pattern_presets
+
+      local hops = neowords.get_word_hops(
+        "\\v,+",
+        "\\v\\.+",
+        "\\v['\"]+", -- '"
+        "\\v[\\(\\)\\{\\}\\[\\]]+", -- (){}[]
+        -- "\\v[\\(\\)]+", -- ()
+        -- "\\v[\\[\\]]+", -- []
+        -- "\\v[\\{\\}]+", -- {}
+        presets.camel_case,
+        presets.hex_color,
+        presets.number,
+        presets.snake_case,
+        presets.upper_case
+      )
+
+      vim.keymap.set({ "n", "x", "o" }, "w", hops.forward_start)
+      vim.keymap.set({ "n", "x", "o" }, "e", hops.forward_end)
+      vim.keymap.set({ "n", "x", "o" }, "b", hops.backward_start)
+      vim.keymap.set({ "n", "x", "o" }, "ge", hops.backward_end)
+    end,
+  },
 }
