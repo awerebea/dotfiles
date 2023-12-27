269bcc943022fab7f0216b36502858505c76581d
awerebea <awerebea.21@gmail.com>
2023-12-27 12:28:21 +0400

    NVIM Add improved-ft plugin

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -625,4 +625,32 @@ return {
     opts = {},
   },
   { "wellle/targets.vim", event = "VeryLazy" },
+  {
+    "backdround/improved-ft.nvim",
+    event = "VeryLazy",
+    config = function()
+      local ft = require "improved-ft"
+      ft.setup {
+        -- Maps default f/F/t/T/;/, keys, default: false
+        use_default_mappings = true,
+        -- Ignores case of the given characters, default: false.
+        ignore_char_case = false,
+        -- Takes a last jump direction into account during repetition jumps, default: false.
+        use_relative_repetition = false,
+      }
+
+      local imap = function(key, fn, description)
+        vim.keymap.set("i", key, fn, { desc = description })
+      end
+
+      imap("<M-f>", ft.hop_forward_to_char, "Hop forward to a given char")
+      imap("<M-F>", ft.hop_backward_to_char, "Hop forward to a given char")
+
+      imap("<M-t>", ft.hop_forward_to_pre_char, "Hop forward before a given char")
+      imap("<M-T>", ft.hop_backward_to_pre_char, "Hop forward before a given char")
+
+      imap("<M-;>", ft.repeat_forward, "Repeat hop forward to a last given char")
+      imap("<M-,>", ft.repeat_backward, "Repeat hop backward to a last given char")
+    end,
+  },
 }
