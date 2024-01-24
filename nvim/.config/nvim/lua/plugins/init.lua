1a634530fe26035359988802174df682ca9c95eb
awerebea <awerebea.21@gmail.com>
2024-01-24 11:06:20 +0400

    NVIM Add scope and modes options to deadcolumn plugin settings

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -387,7 +387,27 @@ return {
       require("treesj").setup(opts)
     end,
   },
-  { "Bekaboo/deadcolumn.nvim", event = "VeryLazy" },
+  {
+    "Bekaboo/deadcolumn.nvim",
+    event = "VeryLazy",
+    opts = {
+      scope = function()
+        local max = nil
+        max = 0
+        for i = -500, 500 do -- number of lines before and after the current line
+          local len = vim.fn.strdisplaywidth(vim.fn.getline(vim.fn.line "." + i))
+          if len > max then
+            max = len
+          end
+        end
+        return max
+      end,
+      -- -@type string[]|fun(mode: string): boolean
+      modes = function(mode)
+        return mode:find "^[icntRss\x13]" ~= nil
+      end,
+    },
+  },
   {
     "SmiteshP/nvim-navbuddy",
     keys = {
