5f2cd9128644ddf361b0742c3c9d8c9e0856c620
awerebea <awerebea.21@gmail.com>
2024-02-17 10:54:13 +0400

    NVIM Disable hover keymap in nvim-ufo module, use lsp/keymaps instead

--- nvim/.config/nvim/lua/plugins/nvim-ufo.lua
+++ nvim/.config/nvim/lua/plugins/nvim-ufo.lua
@@ -71,12 +71,12 @@ return {
     vim.keymap.set("n", "zR", require("ufo").openAllFolds)
     vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
     vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
-    vim.keymap.set("n", "K", function()
-      local winid = require("ufo").peekFoldedLinesUnderCursor()
-      if not winid then
-        -- vim.lsp.buf.hover()
-        vim.cmd [[ Lspsaga hover_doc ++keep ]]
-      end
-    end)
+    -- vim.keymap.set("n", "K", function()
+    --   local winid = require("ufo").peekFoldedLinesUnderCursor()
+    --   if not winid then
+    --     -- vim.lsp.buf.hover()
+    --     vim.cmd [[ Lspsaga hover_doc ++keep ]]
+    --   end
+    -- end)
   end,
 }
