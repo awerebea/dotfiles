a48e439f82c706ecd88203bc3ccd58dec0790147
awerebea <awerebea.21@gmail.com>
2024-04-23 12:06:43 +0400

    NVIM Set the current line only as visible scope for deadcolumn.nvim

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -391,10 +391,11 @@ return {
     "Bekaboo/deadcolumn.nvim",
     event = "VeryLazy",
     opts = {
+      -- scope = "visible",
       scope = function()
         local max = nil
         max = 0
-        for i = -500, 500 do -- number of lines before and after the current line
+        for i = -0, 0 do -- number of lines before and after the current line
           local len = vim.fn.strdisplaywidth(vim.fn.getline(vim.fn.line "." + i))
           if len > max then
             max = len
