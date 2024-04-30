a7c768db0b395d89085b47793364101407df2218
awerebea <awerebea.21@gmail.com>
2024-04-30 11:33:05 +0400

    NVIM Update settings and enable dressing plugin

--- nvim/.config/nvim/lua/plugins/dressing.lua
+++ nvim/.config/nvim/lua/plugins/dressing.lua
@@ -1,9 +1,25 @@
 return {
   "stevearc/dressing.nvim",
-  enabled = false,
+  enabled = true,
   event = "VeryLazy",
   opts = {
-    input = { relative = "editor" },
+    input = {
+      insert_only = false,
+      start_in_insert = true,
+      relative = "editor",
+    },
+    mappings = {
+      n = {
+        ["<Esc>"] = "Close",
+        ["<CR>"] = "Confirm",
+      },
+      i = {
+        ["<C-c>"] = "Close",
+        ["<CR>"] = "Confirm",
+        ["<Up>"] = "HistoryPrev",
+        ["<Down>"] = "HistoryNext",
+      },
+    },
     select = {
       backend = { "telescope", "fzf", "builtin" },
     },
