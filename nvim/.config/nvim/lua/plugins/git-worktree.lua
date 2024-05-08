fd53c343ba4f08cf905c5080d7f21cd5e65b37ba
awerebea <awerebea.21@gmail.com>
2024-05-08 11:58:23 +0400

    Revert "NVIM Switch to fork to manage Telescope incompatibility, git-worktree"
    This reverts commit af34b1445634b02caa254dbf30b39a5ea578edfd.

--- nvim/.config/nvim/lua/plugins/git-worktree.lua
+++ nvim/.config/nvim/lua/plugins/git-worktree.lua
@@ -1,6 +1,5 @@
 return {
-  "awerebea/git-worktree.nvim",
-  branch = "handle_changes_in_telescope_api",
+  "ThePrimeagen/git-worktree.nvim",
   opts = {},
   config = function()
     require("telescope").load_extension "git_worktree"
