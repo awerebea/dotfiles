ef8b99b9cfed111d330c96ae2a82283459d52ebb
awerebea <awerebea.21@gmail.com>
2024-05-09 00:24:36 +0400

    NVIM Update the incline settings
    Let the floating window to overlap window borders.

--- nvim/.config/nvim/lua/plugins/incline.lua
+++ nvim/.config/nvim/lua/plugins/incline.lua
@@ -2,6 +2,9 @@ return {
   "b0o/incline.nvim",
   enabled = true,
   event = "VeryLazy",
-  opts = { hide = { cursorline = "focused_win" } },
+  opts = {
+    hide = { cursorline = "focused_win" },
+    window = { margin = { vertical = 0 }, overlap = { borders = true } },
+  },
   config = true,
 }
