3353892db518d257bc0d031541d2336643c49b38
awerebea <awerebea.21@gmail.com>
2024-05-10 13:34:04 +0400

    NVIM Change the background color of the floating window, incline plugin

--- nvim/.config/nvim/lua/plugins/incline.lua
+++ nvim/.config/nvim/lua/plugins/incline.lua
@@ -5,6 +5,18 @@ return {
   opts = {
     hide = { cursorline = "focused_win" },
     window = { margin = { vertical = 0 }, overlap = { borders = true } },
+    render = function(props)
+      local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+      local modified = vim.bo[props.buf].modified
+      return {
+        filename,
+        modified and { " *", guifg = "#888888", gui = "bold" } or "",
+        guibg = "#292e42",
+      }
+    end,
   },
-  config = true,
+  config = function(_, opts)
+    require("incline").setup(opts)
+    vim.api.nvim_set_hl(0, "InclineNormal", { bg = "#292e42" })
+  end,
 }
