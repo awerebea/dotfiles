355496e9755fbae4ba97eb1a3d73e018156788ee
awerebea <awerebea.21@gmail.com>
2024-05-14 01:42:57 +0400

    NVIM Handle optional possession arguments by helper functions

--- nvim/.config/nvim/lua/plugins/possession.lua
+++ nvim/.config/nvim/lua/plugins/possession.lua
@@ -13,23 +13,6 @@ return {
   config = function()
     -- don't save current directory in session to avoid erros and dead buffers when loading session
     vim.opt.sessionoptions:remove { "curdir" }
-    require("possession").setup {
-      session_name_encode = true,
-      prompt_no_cr = true, -- don't add a carriage return to the prompt
-      autosave = { current = true },
-      plugins = { delete_hidden_buffers = { hooks = {} } },
-      telescope = {
-        list = {
-          default_action = "load",
-          mappings = {
-            save = { n = "<c-s>", i = "<c-s>" },
-            load = { n = "<c-l>", i = "<c-l>" },
-            delete = { n = "<c-x>", i = "<c-x>" },
-            rename = { n = "<c-r>", i = "<c-r>" },
-          },
-        },
-      },
-    }
 
     local sep = package.config:sub(1, 1) -- Returns "\\" on Windows, "/" on Unix-like systems
 
@@ -55,10 +38,48 @@ return {
     local function collapse_path(session_path)
       local path = session_path:gsub("^" .. vim.loop.os_homedir(), "~")
       for alias, alias_path in pairs(path_aliases) do
-        path = path:gsub("^" .. alias_path, alias)
+        path = (path:gsub("^" .. alias_path, alias))
       end
       return path
     end
+
+    require("possession").setup {
+      session_name_encode = true,
+      prompt_no_cr = true, -- don't add a carriage return to the prompt
+      autosave = {
+        current = true,
+        -- tmp = false, -- or fun(): boolean
+        tmp = function()
+          local ignore_filetypes = {
+            "git",
+            "gitcommit",
+            "gitconfig",
+            "gitrebase",
+            "gitsendemail",
+          }
+          local buf_filetype = vim.bo.filetype
+          for _, str in ipairs(ignore_filetypes) do
+            if str == buf_filetype then
+              return false
+            end
+          end
+          return true
+        end,
+        tmp_name = "autosave_" .. collapse_path(vim.fn.getcwd(-1, -1)),
+      },
+      plugins = { delete_hidden_buffers = { hooks = {} } },
+      telescope = {
+        list = {
+          default_action = "load",
+          mappings = {
+            save = { n = "<c-s>", i = "<c-s>" },
+            load = { n = "<c-l>", i = "<c-l>" },
+            delete = { n = "<c-x>", i = "<c-x>" },
+            rename = { n = "<c-r>", i = "<c-r>" },
+          },
+        },
+      },
+    }
 
     local function get_session_file(session_name)
       return vim.fn.stdpath "data" .. sep .. "possession" .. sep .. session_name .. ".json"
@@ -69,7 +90,7 @@ return {
       vim.notify "closed all"
     end
 
-    local function handle_current_cwd_session(cmd)
+    local function handle_current_cwd_session(cmd, args)
       local session_cwd = vim.fn.getcwd(-1, -1)
       local session_name = collapse_path(session_cwd)
       local session_file = get_session_file(require("utils").url_encode(session_name))
@@ -84,12 +105,12 @@ return {
       elseif cmd == "save" then
         -- close_neo_tree()
         -- Overwrite without confirmation
-        require("possession").save(session_name, { no_confirm = true })
+        require("possession").save(session_name, args)
         print("Session CWD is: " .. session_cwd)
       end
     end
 
-    local function save_session()
+    local function save_session(args)
       local session_cwd = vim.fn.getcwd(-1, -1)
       local parts = {}
       if session_cwd then
@@ -100,7 +121,7 @@ return {
         function(session_name)
           if session_name ~= "" then
             -- Overwrite without confirmation
-            require("possession").save(session_name, { no_confirm = true })
+            require("possession").save(session_name, args)
           end
         end
       )
@@ -136,12 +157,16 @@ return {
             return
           end
         end
-        handle_current_cwd_session "save"
-        if vim.g.CWD_initial ~= nil and vim.g.CWD_initial ~= vim.fn.getcwd(-1, -1) then
-          vim.api.nvim_set_current_dir(vim.g.CWD_initial)
-          -- Overwrite without confirmation
-          require("possession").save(collapse_path(vim.g.CWD_initial), { no_confirm = true })
+        if
+          require("possession.session").get_session_name() ~= collapse_path(vim.fn.getcwd(-1, -1))
+        then
+          handle_current_cwd_session("save", { no_confirm = false })
         end
+        -- if vim.g.CWD_initial ~= nil and vim.g.CWD_initial ~= vim.fn.getcwd(-1, -1) then
+        --   vim.api.nvim_set_current_dir(vim.g.CWD_initial)
+        --   -- Overwrite without confirmation
+        --   require("possession").save(collapse_path(vim.g.CWD_initial), { no_confirm = false })
+        -- end
       end,
     })
 
@@ -155,13 +180,13 @@ return {
       handle_current_cwd_session "load"
     end, { desc = "Load the current CWD session" })
     vim.keymap.set("n", "<leader>qS", function()
-      handle_current_cwd_session "save"
+      handle_current_cwd_session("save", { no_confirm = true })
     end, { desc = "Save the current CWD session" })
     vim.keymap.set("n", "<leader>qD", function()
       handle_current_cwd_session "delete"
     end, { desc = "Delete the current CWD session" })
     vim.keymap.set("n", "<leader>qs", function()
-      save_session()
+      save_session { no_confirm = false }
     end, { desc = "Save session" })
 
     require("telescope").load_extension "possession"
