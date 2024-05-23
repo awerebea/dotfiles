100adf013630a567e048d8d7fffbcc0a7d849c65
awerebea <awerebea.21@gmail.com>
2024-05-23 11:38:24 +0400

    NVIM Update flash plugin config
    Enable keymaps to toggle flash search and jump.
    Use flash instead of leap plugin.

--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -30,7 +30,7 @@ return {
         -- This allows using those keys directly after the motion
         label = { exclude = "hjkliardcxsbwerRpy" },
       },
-      search = { enabled = false },
+      search = { enabled = false, jump = { history = true, register = true, nohlsearch = false } },
     },
     label = {
       rainbow = {
@@ -41,13 +41,13 @@ return {
   },
   -- stylua: ignore
   -- keys are disabled because leap plugin is used instead
-  -- keys = {
-  --   { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-  --   { "S", mode = { "n", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-  --   { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-  --   { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-  --   { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-  -- },
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    -- { "S", mode = { "n", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
   config = function(_, opts)
     require("flash").setup(opts)
     -- Use tha same highlight color as LeapLabelPrimary
