ed2b260cd0fa1e4824b3e267f10797882247c46a
awerebea <awerebea.21@gmail.com>
2024-05-28 14:51:58 +0400

    NVIM Fix match sensitivity in vim-fuzzysearch plugin
    Switch to fork.
    ref: https://github.com/awerebea/vim-fuzzysearch/tree/match_sensitivity

--- nvim/.config/nvim/lua/plugins/vim-fuzzysearch.lua
+++ nvim/.config/nvim/lua/plugins/vim-fuzzysearch.lua
@@ -1,13 +1,17 @@
 return {
-  "ggVGc/vim-fuzzysearch",
+  "awerebea/vim-fuzzysearch", -- Temporary switch to fork
+  branch = "match_sensitivity",
   cmd = "FuzzySearch",
   keys = { "/", "<M-/>" },
-  config = function()
+  init = function()
     vim.g.fuzzysearch_prompt = "fuzzy /"
     vim.g.fuzzysearch_hlsearch = 1
     vim.g.fuzzysearch_ignorecase = 1
     vim.g.fuzzysearch_max_history = 30
     vim.g.fuzzysearch_match_spaces = 0
+    vim.g.fuzzysearch_sensitivity = 0
+  end,
+  config = function()
     vim.keymap.set("n", "<M-/>", "/", { noremap = true, desc = "Regex Search" })
     vim.keymap.set("n", "/", "<Cmd>FuzzySearch<CR>", { noremap = false, desc = "Fuzzy Search" })
   end,
