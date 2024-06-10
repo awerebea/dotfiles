6a1f161303f1b3eb9855accef286853ad2066a8c
awerebea <awerebea.21@gmail.com>
2024-06-10 23:33:23 +0400

    NVIM Use `sticky` option for move_cursor, nvim-surround
    ref: https://github.com/kylechui/nvim-surround/releases/tag/v2.2.0

--- nvim/.config/nvim/lua/plugins/nvim-surround.lua
+++ nvim/.config/nvim/lua/plugins/nvim-surround.lua
@@ -7,6 +7,7 @@ return {
       -- override default mapping to avoid conflict with mini.splitjoin plugin
       visual_line = "<leader>gS",
     },
+    move_cursor = "sticky", -- the cursor will "stick" to the current character on surround action
   },
   config = function(_, opts)
     require("nvim-surround").setup(opts)
