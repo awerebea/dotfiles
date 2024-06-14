59be840d3dcb207d81b15aa16ec8997aedc8c5f2
awerebea <awerebea.21@gmail.com>
2024-06-14 16:59:43 +0400

    WIN Autoformat PowerShell profile

--- Win/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Win/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,16 @@
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/awerebea.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/awerebea.omp.yaml" | Invoke-Expression
 
 # Vi mode
 Set-PSReadlineOption -EditMode Vi
 # This example emits a cursor change VT escape in response to a Vi mode change.
-function OnViModeChange {
-    if ($args[0] -eq 'Command') {
+function OnViModeChange
+{
+    if ($args[0] -eq 'Command')
+    {
         # Set the cursor to a blinking block.
         Write-Host -NoNewLine "`e[1 q"
-    } else {
+    } else
+    {
         # Set the cursor to a blinking line.
         Write-Host -NoNewLine "`e[5 q"
     }
@@ -21,12 +24,14 @@ Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $Function:OnVi
 # for `choco` will not function.
 # See https://ch0.co/tab-completion for details.
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
+if (Test-Path($ChocolateyProfile))
+{
     Import-Module "$ChocolateyProfile"
 }
 
 # Catppuccin theme
-if (-not (Get-Module -ListAvailable -Name Catppuccin)) {
+if (-not (Get-Module -ListAvailable -Name Catppuccin))
+{
     $MyDocumentsLocation = [Environment]::GetFolderPath('MyDocuments')
     New-Item -Type Directory -Path "$MyDocumentsLocation\PowerShell\Modules" -ErrorAction SilentlyContinue
     git clone https://github.com/catppuccin/powershell.git "$MyDocumentsLocation\PowerShell\Modules\Catppuccin"
@@ -127,96 +132,297 @@ Set-PSReadLineKeyHandler -Chord "Alt+l" -Function ClearScreen
 
 # Aliases (alias-like functions)
 # Git
-Remove-Alias -Force -Name g 2>&1 | out-null; function g() { git $args }
-Remove-Alias -Force -Name ga 2>&1 | out-null; function ga() { git add $args }
-Remove-Alias -Force -Name gaa 2>&1 | out-null; function gaa() { git add --all $args }
-Remove-Alias -Force -Name gapa 2>&1 | out-null; function gapa() { git add --patch $args }
-Remove-Alias -Force -Name gau 2>&1 | out-null; function gau() { git add --update $args }
-Remove-Alias -Force -Name gb 2>&1 | out-null; function gb() { git branch $args }
-Remove-Alias -Force -Name gbD 2>&1 | out-null; function gbD() { git branch -D $args }
-Remove-Alias -Force -Name gba 2>&1 | out-null; function gba() { git branch -a $args }
-Remove-Alias -Force -Name gbd 2>&1 | out-null; function gbd() { git branch -d $args }
-Remove-Alias -Force -Name gc 2>&1 | out-null; function gc() { git commit -v $args }
-Remove-Alias -Force -Name gc! 2>&1 | out-null; function gc!() { git commit -v --amend $args }
-Remove-Alias -Force -Name gcl 2>&1 | out-null; function gcl() { git clone --recurse-submodules $args }
-Remove-Alias -Force -Name gcn! 2>&1 | out-null; function gcn!() { git commit -v --no-edit --amend $args }
-Remove-Alias -Force -Name gco 2>&1 | out-null; function gco() { git checkout $args }
-Remove-Alias -Force -Name gcp 2>&1 | out-null; function gcp() { git cherry-pick $args }
-Remove-Alias -Force -Name gcpa 2>&1 | out-null; function gcpa() { git cherry-pick --abort $args }
-Remove-Alias -Force -Name gcpc 2>&1 | out-null; function gcpc() { git cherry-pick --continue $args }
-Remove-Alias -Force -Name gcpd! 2>&1 | out-null; function gcpd!() { $env:GIT_COMMITTER_DATE = git log -n 1 --format=%aD; git commit --amend $args; Remove-Item Env:GIT_COMMITTER_DATE }
-Remove-Alias -Force -Name gcpdn! 2>&1 | out-null; function gcpdn!() { $env:GIT_COMMITTER_DATE = git log -n 1 --format=%aD; git commit --amend --no-edit $args; Remove-Item Env:GIT_COMMITTER_DATE }
-Remove-Alias -Force -Name gcud! 2>&1 | out-null; function gcud!() { git commit -v --amend --date=now $args }
-Remove-Alias -Force -Name gcudn! 2>&1 | out-null; function gcudn!() { git commit --amend --date=now --no-edit $args }
-Remove-Alias -Force -Name gd 2>&1 | out-null; function gd() { git diff $args }
-Remove-Alias -Force -Name gdca 2>&1 | out-null; function gdca() { git diff --cached $args }
-Remove-Alias -Force -Name gdcw 2>&1 | out-null; function gdcw() { git diff --cached --word-diff $args }
-Remove-Alias -Force -Name gds 2>&1 | out-null; function gds() { git diff --staged $args }
-Remove-Alias -Force -Name ggsup 2>&1 | out-null; function ggsup() { git branch --set-upstream-to=origin/$(git_current_branch) $args }
-Remove-Alias -Force -Name gl 2>&1 | out-null; function gl() { git pull $args }
-Remove-Alias -Force -Name glg 2>&1 | out-null; function glg() { git log --stat $args }
-Remove-Alias -Force -Name glgp 2>&1 | out-null; function glgp() { git log --stat -p $args }
-Remove-Alias -Force -Name glo 2>&1 | out-null; function glo() { git log --oneline --decorate $args }
-Remove-Alias -Force -Name gloga 2>&1 | out-null; function gloga() { git log --oneline --decorate --graph --all $args }
-Remove-Alias -Force -Name glpf 2>&1 | out-null; function glpf() { git log --pretty=fuller $args }
-Remove-Alias -Force -Name gm 2>&1 | out-null; function gm() { git merge $args }
-Remove-Alias -Force -Name gma 2>&1 | out-null; function gma() { git merge --abort $args }
-Remove-Alias -Force -Name gp 2>&1 | out-null; function gp() { git push $args }
-Remove-Alias -Force -Name gpf 2>&1 | out-null; function gpf() { git push --force-with-lease $args }
-Remove-Alias -Force -Name gpf! 2>&1 | out-null; function gpf!() { git push --force $args }
-Remove-Alias -Force -Name gpsup 2>&1 | out-null; function gpsup() { git push --set-upstream origin $(git_current_branch) $args }
-Remove-Alias -Force -Name gra 2>&1 | out-null; function gra() { git remote add $args }
-Remove-Alias -Force -Name grb 2>&1 | out-null; function grb() { git rebase $args }
-Remove-Alias -Force -Name grba 2>&1 | out-null; function grba() { git rebase --abort $args }
-Remove-Alias -Force -Name grbc 2>&1 | out-null; function grbc() { git rebase --continue $args }
-Remove-Alias -Force -Name grbi 2>&1 | out-null; function grbi() { git rebase -i $args }
-Remove-Alias -Force -Name grbipd 2>&1 | out-null; function grbipd() { git rebase -i --committer-date-is-author-date $args }
-Remove-Alias -Force -Name grbpd 2>&1 | out-null; function grbpd() { git rebase --committer-date-is-author-date $args }
-Remove-Alias -Force -Name grbpdrm 2>&1 | out-null; function grbpd() { git rebase --committer-date-is-author-date --rebase-merges $args }
-Remove-Alias -Force -Name grev 2>&1 | out-null; function grev() { git revert $args }
-Remove-Alias -Force -Name grh 2>&1 | out-null; function grh() { git reset $args }
-Remove-Alias -Force -Name grhh 2>&1 | out-null; function grhh() { git reset --hard $args }
-Remove-Alias -Force -Name grs 2>&1 | out-null; function grs() { git restore $args }
-Remove-Alias -Force -Name grv 2>&1 | out-null; function grv() { git remote -v $args }
-Remove-Alias -Force -Name gss 2>&1 | out-null; function gss() { git status -s $args }
-Remove-Alias -Force -Name gst 2>&1 | out-null; function gst() { git status $args }
-Remove-Alias -Force -Name gsta 2>&1 | out-null; function gsta() { git stash push $args }
-Remove-Alias -Force -Name gstaa 2>&1 | out-null; function gstaa() { git stash apply $args }
-Remove-Alias -Force -Name gstc 2>&1 | out-null; function gstc() { git stash clear $args }
-Remove-Alias -Force -Name gstd 2>&1 | out-null; function gstd() { git stash drop $args }
-Remove-Alias -Force -Name gstl 2>&1 | out-null; function gstl() { git stash list $args }
-Remove-Alias -Force -Name gstp 2>&1 | out-null; function gstp() { git stash pop $args }
-Remove-Alias -Force -Name gsts 2>&1 | out-null; function gsts() { git stash show --text $args }
-Remove-Alias -Force -Name gsts 2>&1 | out-null; function gsts() { git stash show --text $args }
-Remove-Alias -Force -Name gsu 2>&1 | out-null; function gsu() { git submodule update $args }
-Remove-Alias -Force -Name gsw 2>&1 | out-null; function gsw() { git switch $args }
-Remove-Alias -Force -Name gswc 2>&1 | out-null; function gswc() { git switch -c $args }
-Remove-Alias -Force -Name gupv 2>&1 | out-null; function gupv() { git pull --rebase -v $args }
+Remove-Alias -Force -Name g 2>&1 | out-null; function g()
+{
+    git $args
+}
+Remove-Alias -Force -Name ga 2>&1 | out-null; function ga()
+{
+    git add $args
+}
+Remove-Alias -Force -Name gaa 2>&1 | out-null; function gaa()
+{
+    git add --all $args
+}
+Remove-Alias -Force -Name gapa 2>&1 | out-null; function gapa()
+{
+    git add --patch $args
+}
+Remove-Alias -Force -Name gau 2>&1 | out-null; function gau()
+{
+    git add --update $args
+}
+Remove-Alias -Force -Name gb 2>&1 | out-null; function gb()
+{
+    git branch $args
+}
+Remove-Alias -Force -Name gbD 2>&1 | out-null; function gbD()
+{
+    git branch -D $args
+}
+Remove-Alias -Force -Name gba 2>&1 | out-null; function gba()
+{
+    git branch -a $args
+}
+Remove-Alias -Force -Name gbd 2>&1 | out-null; function gbd()
+{
+    git branch -d $args
+}
+Remove-Alias -Force -Name gc 2>&1 | out-null; function gc()
+{
+    git commit -v $args
+}
+Remove-Alias -Force -Name gc! 2>&1 | out-null; function gc!()
+{
+    git commit -v --amend $args
+}
+Remove-Alias -Force -Name gcl 2>&1 | out-null; function gcl()
+{
+    git clone --recurse-submodules $args
+}
+Remove-Alias -Force -Name gcn! 2>&1 | out-null; function gcn!()
+{
+    git commit -v --no-edit --amend $args
+}
+Remove-Alias -Force -Name gco 2>&1 | out-null; function gco()
+{
+    git checkout $args
+}
+Remove-Alias -Force -Name gcp 2>&1 | out-null; function gcp()
+{
+    git cherry-pick $args
+}
+Remove-Alias -Force -Name gcpa 2>&1 | out-null; function gcpa()
+{
+    git cherry-pick --abort $args
+}
+Remove-Alias -Force -Name gcpc 2>&1 | out-null; function gcpc()
+{
+    git cherry-pick --continue $args
+}
+Remove-Alias -Force -Name gcpd! 2>&1 | out-null; function gcpd!()
+{
+    $env:GIT_COMMITTER_DATE = git log -n 1 --format=%aD; git commit --amend $args; Remove-Item Env:GIT_COMMITTER_DATE
+}
+Remove-Alias -Force -Name gcpdn! 2>&1 | out-null; function gcpdn!()
+{
+    $env:GIT_COMMITTER_DATE = git log -n 1 --format=%aD; git commit --amend --no-edit $args; Remove-Item Env:GIT_COMMITTER_DATE
+}
+Remove-Alias -Force -Name gcud! 2>&1 | out-null; function gcud!()
+{
+    git commit -v --amend --date=now $args
+}
+Remove-Alias -Force -Name gcudn! 2>&1 | out-null; function gcudn!()
+{
+    git commit --amend --date=now --no-edit $args
+}
+Remove-Alias -Force -Name gd 2>&1 | out-null; function gd()
+{
+    git diff $args
+}
+Remove-Alias -Force -Name gdca 2>&1 | out-null; function gdca()
+{
+    git diff --cached $args
+}
+Remove-Alias -Force -Name gdcw 2>&1 | out-null; function gdcw()
+{
+    git diff --cached --word-diff $args
+}
+Remove-Alias -Force -Name gds 2>&1 | out-null; function gds()
+{
+    git diff --staged $args
+}
+Remove-Alias -Force -Name ggsup 2>&1 | out-null; function ggsup()
+{
+    git branch --set-upstream-to=origin/$(git_current_branch) $args
+}
+Remove-Alias -Force -Name gl 2>&1 | out-null; function gl()
+{
+    git pull $args
+}
+Remove-Alias -Force -Name glg 2>&1 | out-null; function glg()
+{
+    git log --stat $args
+}
+Remove-Alias -Force -Name glgp 2>&1 | out-null; function glgp()
+{
+    git log --stat -p $args
+}
+Remove-Alias -Force -Name glo 2>&1 | out-null; function glo()
+{
+    git log --oneline --decorate $args
+}
+Remove-Alias -Force -Name gloga 2>&1 | out-null; function gloga()
+{
+    git log --oneline --decorate --graph --all $args
+}
+Remove-Alias -Force -Name glpf 2>&1 | out-null; function glpf()
+{
+    git log --pretty=fuller $args
+}
+Remove-Alias -Force -Name gm 2>&1 | out-null; function gm()
+{
+    git merge $args
+}
+Remove-Alias -Force -Name gma 2>&1 | out-null; function gma()
+{
+    git merge --abort $args
+}
+Remove-Alias -Force -Name gp 2>&1 | out-null; function gp()
+{
+    git push $args
+}
+Remove-Alias -Force -Name gpf 2>&1 | out-null; function gpf()
+{
+    git push --force-with-lease $args
+}
+Remove-Alias -Force -Name gpf! 2>&1 | out-null; function gpf!()
+{
+    git push --force $args
+}
+Remove-Alias -Force -Name gpsup 2>&1 | out-null; function gpsup()
+{
+    git push --set-upstream origin $(git_current_branch) $args
+}
+Remove-Alias -Force -Name gra 2>&1 | out-null; function gra()
+{
+    git remote add $args
+}
+Remove-Alias -Force -Name grb 2>&1 | out-null; function grb()
+{
+    git rebase $args
+}
+Remove-Alias -Force -Name grba 2>&1 | out-null; function grba()
+{
+    git rebase --abort $args
+}
+Remove-Alias -Force -Name grbc 2>&1 | out-null; function grbc()
+{
+    git rebase --continue $args
+}
+Remove-Alias -Force -Name grbi 2>&1 | out-null; function grbi()
+{
+    git rebase -i $args
+}
+Remove-Alias -Force -Name grbipd 2>&1 | out-null; function grbipd()
+{
+    git rebase -i --committer-date-is-author-date $args
+}
+Remove-Alias -Force -Name grbpd 2>&1 | out-null; function grbpd()
+{
+    git rebase --committer-date-is-author-date $args
+}
+Remove-Alias -Force -Name grbpdrm 2>&1 | out-null; function grbpd()
+{
+    git rebase --committer-date-is-author-date --rebase-merges $args
+}
+Remove-Alias -Force -Name grev 2>&1 | out-null; function grev()
+{
+    git revert $args
+}
+Remove-Alias -Force -Name grh 2>&1 | out-null; function grh()
+{
+    git reset $args
+}
+Remove-Alias -Force -Name grhh 2>&1 | out-null; function grhh()
+{
+    git reset --hard $args
+}
+Remove-Alias -Force -Name grs 2>&1 | out-null; function grs()
+{
+    git restore $args
+}
+Remove-Alias -Force -Name grv 2>&1 | out-null; function grv()
+{
+    git remote -v $args
+}
+Remove-Alias -Force -Name gss 2>&1 | out-null; function gss()
+{
+    git status -s $args
+}
+Remove-Alias -Force -Name gst 2>&1 | out-null; function gst()
+{
+    git status $args
+}
+Remove-Alias -Force -Name gsta 2>&1 | out-null; function gsta()
+{
+    git stash push $args
+}
+Remove-Alias -Force -Name gstaa 2>&1 | out-null; function gstaa()
+{
+    git stash apply $args
+}
+Remove-Alias -Force -Name gstc 2>&1 | out-null; function gstc()
+{
+    git stash clear $args
+}
+Remove-Alias -Force -Name gstd 2>&1 | out-null; function gstd()
+{
+    git stash drop $args
+}
+Remove-Alias -Force -Name gstl 2>&1 | out-null; function gstl()
+{
+    git stash list $args
+}
+Remove-Alias -Force -Name gstp 2>&1 | out-null; function gstp()
+{
+    git stash pop $args
+}
+Remove-Alias -Force -Name gsts 2>&1 | out-null; function gsts()
+{
+    git stash show --text $args
+}
+Remove-Alias -Force -Name gsts 2>&1 | out-null; function gsts()
+{
+    git stash show --text $args
+}
+Remove-Alias -Force -Name gsu 2>&1 | out-null; function gsu()
+{
+    git submodule update $args
+}
+Remove-Alias -Force -Name gsw 2>&1 | out-null; function gsw()
+{
+    git switch $args
+}
+Remove-Alias -Force -Name gswc 2>&1 | out-null; function gswc()
+{
+    git switch -c $args
+}
+Remove-Alias -Force -Name gupv 2>&1 | out-null; function gupv()
+{
+    git pull --rebase -v $args
+}
 
-Remove-Alias -Force -Name gcm 2>&1 | out-null; function gcm() { git checkout main || git checkout master }
+Remove-Alias -Force -Name gcm 2>&1 | out-null; function gcm()
+{
+    git checkout main || git checkout master
+}
 
 New-Alias v nvim
 
 New-Alias lg lazygit
 
 # Python Virtual Environment activation
-function Activate-VirtualEnvironment {
-        param (
-                [string]$venvName = ".venv"
-        )
+function Enter-VirtualEnvironment
+{
+    param (
+        [string]$venvName = ".venv"
+    )
 
-        # Create the virtual environment
-        python -m venv $venvName
+    # Create the virtual environment
+    python -m venv $venvName
 
-        # Activate the virtual environment
-        $activateScript = Join-Path $venvName "Scripts\Activate.ps1"
-        if (Test-Path $activateScript) {
-                . $activateScript
-        } else {
-                Write-Error "Failed to find Activate.ps1 script in the virtual environment."
-        }
+    # Activate the virtual environment
+    $activateScript = Join-Path $venvName "Scripts\Activate.ps1"
+    if (Test-Path $activateScript)
+    {
+        . $activateScript
+    } else
+    {
+        Write-Error "Failed to find Activate.ps1 script in the virtual environment."
+    }
 }
-Set-Alias -Name activate -Value Activate-VirtualEnvironment
+Set-Alias -Name activate -Value Enter-VirtualEnvironment
 
 # gmom='git merge origin/$(git_main_branch)'
 # gmtl='git mergetool --no-prompt'
