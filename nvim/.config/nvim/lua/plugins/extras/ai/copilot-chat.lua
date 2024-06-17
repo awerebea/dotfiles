b6c4671e0c54d842f0a0f9528b88a42c3fe663f6
awerebea <awerebea.21@gmail.com>
2024-06-17 11:13:37 +0400

    feat: Add CopilotChat plugin to Neovim configuration
    This commit introduces the CopilotChat plugin to the Neovim
    configuration. The plugin is added with its dependencies and
    associated commands. Keybindings are also set up for the plugin
    under the leader key 'c'. This includes keybindings for help actions
    and prompt actions.

---
+++ nvim/.config/nvim/lua/plugins/extras/ai/copilot-chat.lua
@@ -0,0 +1,72 @@
+return {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  branch = "canary",
+  dependencies = {
+    { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+    { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+  },
+  keys = { "<leader>cch", "<leader>ccp" },
+  cmd = {
+    "CopilotChat",
+    "CopilotChatOpen",
+    "CopilotChatClose",
+    "CopilotChatToggle",
+    "CopilotChatStop",
+    "CopilotChatReset",
+    "CopilotChatSave",
+    "CopilotChatLoad",
+    "CopilotChatDebugInfo",
+  },
+  opts = {
+    debug = false, -- Enable debug logging
+    -- default mappings
+    mappings = {
+      complete = {
+        detail = "Use @<Tab> or /<Tab> for options.",
+        insert = "<Tab>",
+      },
+      close = {
+        normal = "q",
+        insert = "<C-c>",
+      },
+      reset = {
+        normal = "<C-l>",
+        insert = "<C-l>",
+      },
+      submit_prompt = {
+        normal = "<CR>",
+        insert = "<C-m>",
+      },
+      accept_diff = {
+        normal = "<C-y>",
+        insert = "<C-y>",
+      },
+      yank_diff = {
+        normal = "gy",
+      },
+      show_diff = {
+        normal = "gd",
+      },
+      show_system_prompt = {
+        normal = "gp",
+      },
+      show_user_selection = {
+        normal = "gs",
+      },
+    },
+  },
+  config = function(_, opts)
+    require("CopilotChat").setup(opts)
+
+    -- Show help actions with telescope
+    vim.keymap.set("n", "<leader>cch", function()
+      local actions = require "CopilotChat.actions"
+      require("CopilotChat.integrations.telescope").pick(actions.help_actions())
+    end, { desc = "Help actions" })
+    -- Show prompts actions with telescope
+    vim.keymap.set("n", "<leader>ccp", function()
+      local actions = require "CopilotChat.actions"
+      require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+    end, { desc = "Prompt actions" })
+  end,
+}
