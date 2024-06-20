c41345eca6e347e13c561313fcbaef92ab1c4d9b
awerebea <awerebea.21@gmail.com>
2024-06-20 12:38:21 +0400

    NVIM Refactor keymap set and remove unused functions, neoscopes
    This commit refactors the way keymaps are set in the neoscopes.lua file.
    It removes the global functions previously used for setting keymaps and
    replaces them with direct calls to the `telescope.builtin` functions.
    This change makes the code more concise and easier to understand.
    
    Additionally, some unused functions and commented out code have been
    removed to clean up the file. This includes the `neoscopes_find_files`,
    `neoscopes_live_grep`, `neoscopes_live_grep_args`,
    `neoscopes_fuzzy_grep`, and `neoscopes_grep_word` functions.

--- nvim/.config/nvim/lua/plugins/neoscopes.lua
+++ nvim/.config/nvim/lua/plugins/neoscopes.lua
@@ -31,52 +31,6 @@ return {
       return scopes.get_current_paths()
     end
 
-    _G.neoscopes_find_files = function()
-      -- require("plugins.telescope.telescopePickers").prettyFilesPicker {
-      --   picker = "find_files",
-      --   options = { search_dirs = get_search_dirs() },
-      -- }
-      require("telescope.builtin").find_files {
-        search_dirs = get_search_dirs(),
-      }
-    end
-    _G.neoscopes_live_grep = function()
-      -- require("plugins.telescope.telescopePickers").prettyGrepPicker {
-      --   picker = "live_grep",
-      --   options = { search_dirs = get_search_dirs() },
-      -- }
-      require("telescope.builtin").live_grep {
-        search_dirs = get_search_dirs(),
-      }
-    end
-    _G.neoscopes_live_grep_args = function()
-      require("telescope").extensions.live_grep_args.live_grep_args {
-        search_dirs = get_search_dirs(),
-        shorten_path = true,
-        word_match = "-w",
-        only_sort_text = true,
-        search = "",
-      }
-    end
-    _G.neoscopes_fuzzy_grep = function()
-      require("telescope.builtin").grep_string {
-        search_dirs = get_search_dirs(),
-        shorten_path = true,
-        word_match = "-w",
-        only_sort_text = true,
-        search = "",
-      }
-    end
-    _G.neoscopes_grep_word = function()
-      -- require("plugins.telescope.telescopePickers").prettyGrepPicker {
-      --   picker = "grep_string",
-      --   options = { search_dirs = get_search_dirs() },
-      -- }
-      require("telescope.builtin").grep_string {
-        search_dirs = get_search_dirs(),
-      }
-    end
-
     vim.keymap.set("n", "<leader>ff", function()
       require("telescope.builtin").find_files {
         search_dirs = get_search_dirs(),
@@ -89,16 +43,32 @@ return {
       }
     end, { desc = "Find files (menufacture)" })
     vim.keymap.set("n", "<leader>f/", function()
-      neoscopes_live_grep()
+      require("telescope.builtin").live_grep {
+        search_dirs = get_search_dirs(),
+      }
     end, { desc = "Live grep (menufacture)" })
     vim.keymap.set("n", "<leader>f?", function()
-      neoscopes_live_grep_args()
+      require("telescope").extensions.live_grep_args.live_grep_args {
+        search_dirs = get_search_dirs(),
+        shorten_path = true,
+        word_match = "-w",
+        only_sort_text = true,
+        search = "",
+      }
     end, { desc = "Live grep (custom args)" })
     vim.keymap.set("n", "<leader>fg", function()
-      neoscopes_fuzzy_grep()
+      require("telescope.builtin").grep_string {
+        search_dirs = get_search_dirs(),
+        shorten_path = true,
+        word_match = "-w",
+        only_sort_text = true,
+        search = "",
+      }
     end, { desc = "Fuzzy Grep (menufacture)" })
     vim.keymap.set("n", "<leader>fw", function()
-      neoscopes_grep_word()
+      require("telescope.builtin").grep_string {
+        search_dirs = get_search_dirs(),
+      }
     end, { desc = "Find word under cursor (menufacture)" })
   end,
 }
