54757d49198fa61a4118ce0bb56e320b81343efa
awerebea <awerebea.21@gmail.com>
2024-06-20 12:32:20 +0400

    NVIM Enhance Telescope file search with visual selection
    This commit enhances the file search functionality in Neovim. It allows
    the user to use the visually selected text as the default search text
    when searching for files. This is achieved by modifying the keymap
    settings in the 'neoscopes.lua' and 'telescope/init.lua' files. A new
    utility function 'get_visual_selection_text' is also added in the
    'utils/init.lua' file to get the visually selected text.

--- nvim/.config/nvim/lua/utils/init.lua
+++ nvim/.config/nvim/lua/utils/init.lua
@@ -185,4 +185,54 @@ function M.serialize_table(val, name, skipnewlines, depth)
   return tmp
 end
 
+--- Return the visually selected text as an array with an entry for each line
+---
+--- @return string[]|nil lines The selected text as an array of lines.
+function M.get_visual_selection_text()
+  local _, srow, scol = unpack(vim.fn.getpos "v")
+  local _, erow, ecol = unpack(vim.fn.getpos ".")
+
+  -- visual line mode
+  if vim.fn.mode() == "V" then
+    if srow > erow then
+      return vim.api.nvim_buf_get_lines(0, erow - 1, srow, true)
+    else
+      return vim.api.nvim_buf_get_lines(0, srow - 1, erow, true)
+    end
+  end
+
+  -- regular visual mode
+  if vim.fn.mode() == "v" then
+    if srow < erow or (srow == erow and scol <= ecol) then
+      return vim.api.nvim_buf_get_text(0, srow - 1, scol - 1, erow - 1, ecol, {})
+    else
+      return vim.api.nvim_buf_get_text(0, erow - 1, ecol - 1, srow - 1, scol, {})
+    end
+  end
+
+  -- visual block mode
+  if vim.fn.mode() == "\22" then
+    local lines = {}
+    if srow > erow then
+      srow, erow = erow, srow
+    end
+    if scol > ecol then
+      scol, ecol = ecol, scol
+    end
+    for i = srow, erow do
+      table.insert(
+        lines,
+        vim.api.nvim_buf_get_text(
+          0,
+          i - 1,
+          math.min(scol - 1, ecol),
+          i - 1,
+          math.max(scol - 1, ecol),
+          {}
+        )[1]
+      )
+    end
+    return lines
+  end
+end
 return M
