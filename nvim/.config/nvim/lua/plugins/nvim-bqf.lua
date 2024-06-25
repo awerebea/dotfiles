7dbfd96c1efd01e5c0074619eb6800ebd171e67a
awerebea <awerebea.21@gmail.com>
2024-06-25 16:50:45 +0400

    feat(nvim-bqf): disable auto preview
    The configuration for the nvim-bqf plugin has been updated to disable
    the auto preview feature. This change affects the Quickfix window
    behavior in Neovim.

--- nvim/.config/nvim/lua/plugins/nvim-bqf.lua
+++ nvim/.config/nvim/lua/plugins/nvim-bqf.lua
@@ -2,4 +2,7 @@ return {
   "kevinhwang91/nvim-bqf",
   ft = "qf",
   dependencies = { "junegunn/fzf" },
+  config = function()
+    require("bqf").setup { preview = { auto_preview = false } }
+  end,
 }
