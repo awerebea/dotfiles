2595f8f53d3ed68a85e9726031f6da5b245c8b65
awerebea <awerebea.21@gmail.com>
2024-06-25 12:24:13 +0400

    feat(oil): enhance configuration and add gitignore support
    This commit enhances the configuration of the 'oil' plugin. It adds a
    new keybinding for launching the 'Oil' file manager. It also introduces
    a new configuration function that sets up the 'oil' plugin with custom
    view options.
    
    One of the significant changes is the addition of gitignore support. The
    configuration function now checks if a file is gitignored before
    deciding whether to display it in the file explorer. This feature
    improves the usability of the 'Oil' file manager by hiding unnecessary
    files.
    
    The commit also removes the 'VeryLazy' event and the
    'default_file_explorer' option as they are no longer needed with the new
    configuration.

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,10 +1,51 @@
 return {
   "stevearc/oil.nvim",
-  event = "VeryLazy",
-  opts = {
-    default_file_explorer = false,
-  },
+  keys = { { "-", "<Cmd>Oil<CR>", desc = "Oil file manager" } },
+  cmd = "Oil",
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = true,
+  config = function()
+    local git_ignored = setmetatable({}, {
+      __index = function(self, key)
+        local proc = vim.system(
+          { "git", "ls-files", "--ignored", "--exclude-standard", "--others", "--directory" },
+          {
+            cwd = key,
+            text = true,
+          }
+        )
+        local result = proc:wait()
+        local ret = {}
+        if result.code == 0 then
+          for line in vim.gsplit(result.stdout, "\n", { plain = true, trimempty = true }) do
+            -- Remove trailing slash
+            line = line:gsub("/$", "")
+            table.insert(ret, line)
+          end
+        end
+
+        rawset(self, key, ret)
+        return ret
+      end,
+    })
+
+    require("oil").setup {
+      default_file_explorer = false,
+      view_options = {
+        is_hidden_file = function(name, _)
+          -- dotfiles are not considered hidden
+          if vim.startswith(name, ".") then
+            return false
+          end
+          local dir = require("oil").get_current_dir()
+          -- if no local directory (e.g. for ssh connections), always show
+          if not dir then
+            return false
+          end
+          -- Check if file is gitignored
+          return vim.list_contains(git_ignored[dir], name)
+        end,
+      },
+    }
+  end,
 }
