9895b4129e0ac5e78e4251c8ae5462f84b9ef862
awerebea <awerebea.21@gmail.com>
2024-06-28 10:45:58 +0400

    feat(hop.nvim): update plugin and refactor key bindings
    The hop.nvim plugin has been updated from 'awerebea/hop.nvim' to
    'smoka7/hop.nvim'. The event has been changed from 'VeryLazy' to
    'WinEnter'. The key bindings have been refactored to use direct
    function calls instead of custom wrapper functions. The color scheme
    for 'HopNextKey1' and 'HopNextKey2' has also been updated.

--- nvim/.config/nvim/lua/plugins/hop.lua
+++ nvim/.config/nvim/lua/plugins/hop.lua
@@ -1,89 +1,74 @@
 return {
-  "awerebea/hop.nvim",
-  branch = "fix-cursor-on-empty-line",
-  event = "VeryLazy",
-  opts = { keys = "asdfghjklqwertyuiopzxcvbnmASDFGHJKLQWERTYUIOPZXCVBNM" },
+  "smoka7/hop.nvim",
+  event = "WinEnter",
+  opts = { keys = "etovxqpdygfblzhckisuran" },
   config = function(_, opts)
     local hop = require "hop"
     hop.setup(opts)
 
     vim.api.nvim_set_hl(0, "HopNextKey", { fg = "Black", bg = "#ccff88" })
-    vim.api.nvim_set_hl(0, "HopNextKey1", { fg = "Black", bg = "#ff0000" })
-    vim.api.nvim_set_hl(0, "HopNextKey2", { fg = "Black", bg = "#b98300" })
+    vim.api.nvim_set_hl(0, "HopNextKey1", { fg = "Black", bg = "#79ab37" })
+    vim.api.nvim_set_hl(0, "HopNextKey2", { fg = "Black", bg = "#a4e84a" })
     vim.api.nvim_set_hl(0, "HopUnmatched", { fg = "#6c7086" })
 
     local directions = require("hop.hint").HintDirection
     local position = require("hop.hint").HintPosition
-    -- emulation of t/T jump
-    HopTFw = function()
+
+    -- vim.keymap.set("", "<leader><leader><leader>", "<Cmd>HopWord<CR>")
+    vim.keymap.set("", "<leader><leader>W", "<Cmd>HopWordMW<CR>")
+    vim.keymap.set("", "<leader><leader>w", "<Cmd>HopWordAC<CR>")
+    vim.keymap.set("", "<leader><leader>b", "<Cmd>HopWordBC<CR>")
+    vim.keymap.set("", "<leader><leader>f", "<Cmd>HopChar1AC<CR>")
+    vim.keymap.set("", "<leader><leader>F", "<Cmd>HopChar1BC<CR>")
+    vim.keymap.set("", "<leader><leader>t", function()
       hop.hint_char1 {
         direction = directions.AFTER_CURSOR,
         current_line_only = false,
         hint_offset = -1,
       }
-    end
-
-    HopTBw = function()
+    end)
+    vim.keymap.set("", "<leader><leader>T", function()
       hop.hint_char1 {
         direction = directions.BEFORE_CURSOR,
         current_line_only = false,
         hint_offset = 1,
       }
-    end
-
-    HopToEndWordFw = function()
+    end)
+    vim.keymap.set("", "<leader><leader>e", function()
       hop.hint_words {
         hint_position = position.END,
         direction = directions.AFTER_CURSOR,
       }
-    end
-
-    HopToEndWordBw = function()
+    end)
+    vim.keymap.set("", "<leader><leader>ge", function()
       hop.hint_words {
         hint_position = position.END,
         direction = directions.BEFORE_CURSOR,
       }
-    end
-
-    HopAfterEndWordFw = function()
+    end)
+    vim.keymap.set("", "<leader><leader>E", function()
       hop.hint_words {
         hint_position = position.END,
         direction = directions.AFTER_CURSOR,
         hint_offset = 1,
       }
-    end
-
-    HopAfterEndWordBw = function()
+    end)
+    vim.keymap.set("", "<leader><leader>gE", function()
       hop.hint_words {
         hint_position = position.END,
         direction = directions.BEFORE_CURSOR,
         hint_offset = 1,
       }
-    end
-
-    local keymap = vim.keymap.set
-
-    -- keymap("", "<leader><leader><leader>", "<Cmd>HopWord<CR>")
-    keymap("", "<leader><leader>W", "<Cmd>HopWordMW<CR>")
-    keymap("", "<leader><leader>w", "<Cmd>HopWordAC<CR>")
-    keymap("", "<leader><leader>b", "<Cmd>HopWordBC<CR>")
-    keymap("", "<leader><leader>f", "<Cmd>HopChar1AC<CR>")
-    keymap("", "<leader><leader>F", "<Cmd>HopChar1BC<CR>")
-    keymap("", "<leader><leader>t", "<Cmd>lua HopTFw()<CR>")
-    keymap("", "<leader><leader>T", "<Cmd>lua HopTBw()<CR>")
-    keymap("", "<leader><leader>e", "<Cmd>lua HopToEndWordFw()<CR>")
-    keymap("", "<leader><leader>ge", "<Cmd>lua HopToEndWordBw()<CR>")
-    keymap("", "<leader><leader>E", "<Cmd>lua HopAfterEndWordFw()<CR>")
-    keymap("", "<leader><leader>gE", "<Cmd>lua HopAfterEndWordBw()<CR>")
-    keymap("", "<leader><leader>s", "<Cmd>HopChar1<CR>")
-    keymap("", "<leader><leader>S", "<Cmd>HopChar2<CR>")
-    keymap("", "<C-s>", "<Cmd>HopChar2<CR>")
-    keymap("", "<leader><leader>2s", "<Cmd>HopChar2<CR>")
-    keymap("", "<leader><leader>d", "<Cmd>HopChar2<CR>")
-    keymap("", "<leader><leader>J", "<Cmd>HopLineAC<CR>")
-    keymap("", "<leader><leader>K", "<Cmd>HopLineBC<CR>")
-    keymap("", "<leader><leader>j", "<Cmd>HopVerticalAC<CR>")
-    keymap("", "<leader><leader>k", "<Cmd>HopVerticalBC<CR>")
-    keymap("", "<leader><leader>/", "<Cmd>HopPattern<CR>")
+    end)
+    vim.keymap.set("", "<leader><leader>s", "<Cmd>HopChar1<CR>")
+    vim.keymap.set("", "<leader><leader>S", "<Cmd>HopChar2<CR>")
+    vim.keymap.set("", "<C-s>", "<Cmd>HopChar2<CR>")
+    vim.keymap.set("", "<leader><leader>2s", "<Cmd>HopChar2<CR>")
+    vim.keymap.set("", "<leader><leader>d", "<Cmd>HopChar2<CR>")
+    vim.keymap.set("", "<leader><leader>J", "<Cmd>HopLineAC<CR>")
+    vim.keymap.set("", "<leader><leader>K", "<Cmd>HopLineBC<CR>")
+    vim.keymap.set("", "<leader><leader>j", "<Cmd>HopVerticalAC<CR>")
+    vim.keymap.set("", "<leader><leader>k", "<Cmd>HopVerticalBC<CR>")
+    vim.keymap.set("", "<leader><leader>/", "<Cmd>HopPattern<CR>")
   end,
 }
