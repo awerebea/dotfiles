eed2c9ed31435065f784b01159682624177b5fcf
awerebea <awerebea.21@gmail.com>
2024-07-19 18:11:30 +0400

    chore: Update which-key configuration for v3 changes
    Updated which-key configuration to reflect changes introduced in version 3.

--- nvim/.config/nvim/lua/plugins/nvim-spectre.lua
+++ nvim/.config/nvim/lua/plugins/nvim-spectre.lua
@@ -2,11 +2,19 @@ return {
   "windwp/nvim-spectre",
   cmd = { "Spectre" },
   keys = {
-    { "<leader>Ss", "<Cmd>Spectre<CR>" },
+    { "<leader>Ss", "<Cmd>Spectre<CR>", desc = "Replace in workplace" },
     -- search current word
-    { "<leader>Sw", "<Cmd>lua require('spectre').open_visual({select_word=true})<CR>" },
+    {
+      "<leader>Sw",
+      "<Cmd>lua require('spectre').open_visual({select_word=true})<CR>",
+      desc = "Replace word under curson in workplace",
+    },
     -- search in current file
-    { "<leader>Sf", "<Cmd>lua require('spectre').open_file_search()<CR>" },
+    {
+      "<leader>Sf",
+      "<Cmd>lua require('spectre').open_file_search()<CR>",
+      desc = "Replace in current file",
+    },
   },
   config = true,
 }
