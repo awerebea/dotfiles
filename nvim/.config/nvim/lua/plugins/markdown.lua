9dee1301fa91be71613a78e55792c68157eaff86
awerebea <awerebea.21@gmail.com>
2024-07-26 19:51:28 +0400

    feat(markdown): Add markdown.nvim plugin
    This commit introduces the markdown.nvim plugin to the Neovim
    configuration. This plugin is used for rendering markdown files. It also
    adds several dependencies required by the plugin, including mini.icons,
    nvim-web-devicons, and nvim-treesitter.

---
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,11 @@
+return {
+  "MeanderingProgrammer/markdown.nvim",
+  ft = "markdown",
+  main = "render-markdown",
+  opts = {},
+  dependencies = {
+    "echasnovski/mini.icons",
+    "nvim-tree/nvim-web-devicons",
+    "nvim-treesitter/nvim-treesitter",
+  },
+}
