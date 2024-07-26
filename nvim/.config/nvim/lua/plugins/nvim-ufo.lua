3673692585af2f13d93700bf5e8e7e86d3e32661
awerebea <awerebea.21@gmail.com>
2024-07-26 20:36:34 +0400

    feat(nvim-ufo): Enable auto foldcolumn and disable diagnostic
    This commit enables the 'auto:4' setting for 'foldcolumn' in the
    nvim-ufo plugin configuration. This will automatically adjust the
    foldcolumn width up to a maximum of 4 characters.
    
    Additionally, a diagnostic line has been disabled to prevent linting
    warnings about the 'provider_selector' function. This function is
    required for the plugin to function correctly.

--- nvim/.config/nvim/lua/plugins/nvim-ufo.lua
+++ nvim/.config/nvim/lua/plugins/nvim-ufo.lua
@@ -4,6 +4,7 @@ return {
   event = "VeryLazy",
   opts = {
     -- INFO: Uncomment to use treesitter as fold provider, otherwise nvim lsp is used
+    ---@diagnostic disable-next-line
     provider_selector = function(bufnr, filetype, buftype)
       return { "treesitter", "indent" }
     end,
@@ -28,7 +29,7 @@ return {
   },
   init = function()
     vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-    vim.o.foldcolumn = "1" -- '0' is not bad
+    vim.o.foldcolumn = "auto:4" -- '0' is not bad
     vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
     vim.o.foldlevelstart = 99
     vim.o.foldenable = true
