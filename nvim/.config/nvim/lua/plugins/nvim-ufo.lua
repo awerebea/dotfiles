f45d56bed79c43d85f52712832b3afc7c895456e
awerebea <awerebea.21@gmail.com>
2024-07-27 22:50:00 +0400

    feat(nvim-ufo): Update fold separator character
    The fold separator character in the nvim-ufo plugin configuration
    has been updated. The previous character was a space, which has now
    been replaced with a vertical bar (│). This change improves the
    visibility of folded code sections in the Neovim editor.

--- nvim/.config/nvim/lua/plugins/nvim-ufo.lua
+++ nvim/.config/nvim/lua/plugins/nvim-ufo.lua
@@ -28,7 +28,7 @@ return {
     },
   },
   init = function()
-    vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+    vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep:│,foldclose:]]
     vim.o.foldcolumn = "auto:4" -- '0' is not bad
     vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
     vim.o.foldlevelstart = 99
