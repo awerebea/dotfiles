e4a2830932cc9a0062c519375cebc7d140482521
awerebea <awerebea.21@gmail.com>
2024-07-27 10:36:46 +0400

    feat(vim-fuzzysearch): Limit search sensitivity by 5
    The sensitivity of the vim-fuzzysearch plugin has been limited by 5
    characters. This change will make the search more precise and reduce the
    number of irrelevant results.

--- nvim/.config/nvim/lua/plugins/vim-fuzzysearch.lua
+++ nvim/.config/nvim/lua/plugins/vim-fuzzysearch.lua
@@ -9,7 +9,7 @@ return {
     vim.g.fuzzysearch_ignorecase = 1
     vim.g.fuzzysearch_max_history = 30
     vim.g.fuzzysearch_match_spaces = 0
-    vim.g.fuzzysearch_sensitivity = 0
+    vim.g.fuzzysearch_sensitivity = 5
   end,
   config = function()
     -- vim.keymap.set("n", "<M-/>", "/", { noremap = true, desc = "Regex Search" })
