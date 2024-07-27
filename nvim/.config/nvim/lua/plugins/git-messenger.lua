934c6bcc67782bccf749e6fbfa5e382a89ee1427
awerebea <awerebea.21@gmail.com>
2024-07-27 10:57:40 +0400

    feat(git-messenger): Change hotkey binding
    The hotkey binding for git-messenger has been updated. The new binding
    now triggers the GitMessenger command directly, improving usability and
    efficiency.

--- nvim/.config/nvim/lua/plugins/git-messenger.lua
+++ nvim/.config/nvim/lua/plugins/git-messenger.lua
@@ -1,7 +1,7 @@
 return {
   "rhysd/git-messenger.vim",
   cmd = "GitMessenger",
-  keys = { "<leader>gm", desc = "Git messenger" },
+  keys = { { "<leader>gm", "<Cmd>GitMessenger<CR>", desc = "GitMessenger" } },
   config = function()
     vim.g.git_messenger_always_into_popup = true
     vim.g.git_messenger_floating_win_opts = { border = "single" }
