13a68046681fa2a390f063b827085b3453fc05eb
awerebea <awerebea.21@gmail.com>
2024-08-11 00:30:17 +0400

    feat(nvim): Update filetype settings for JavaScript
    - Removed JavaScript from the list of filetypes with noexpandtab
      setting.
    - Added JavaScript to the list of filetypes with expandtab and
      tabstop=2.
    - Ensured consistent indentation settings for JavaScript files.

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -171,10 +171,10 @@ augroup filetypeRelatedSettings
   autocmd BufRead,BufNewFile *.tfstate,*.json.tpl* set ft=json
   autocmd FileType bash,sh,json*,dockerfile,python,cmake
     \ setlocal tabstop=4 softtabstop=4 shiftwidth=4 expandtab
-  autocmd FileType javascript,c,make,cpp,**/cpp.snippets,gitignore,gitconfig
+  autocmd FileType c,make,cpp,**/cpp.snippets,gitignore,gitconfig
     \ setlocal tabstop=4 softtabstop=4 shiftwidth=4 noexpandtab
   autocmd FileType vim,zsh,tmux,conf,nginx,ruby,gitcommit,yaml,yaml.ansible,helm,lua,hcl,terraform,
-                  \toml
+    \toml,javascript
     \ setlocal tabstop=2 softtabstop=2 shiftwidth=2 expandtab
   autocmd FileType ps1,psm1,psd1,ps1xml
     \ setlocal tabstop=4 softtabstop=4 shiftwidth=4 expandtab textwidth=99
