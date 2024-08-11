a0bc9e295414382b345902f66d64ff2fbf7b523f
awerebea <awerebea.21@gmail.com>
2024-08-11 13:31:55 +0400

    feat(startup): Remove komorebic startup script
    Removed the komorebic startup script from the PowerShell startup file.
    This change simplifies the startup process by eliminating an unnecessary
    script execution.

--- Win/Startup/startup.ps1
+++ Win/Startup/startup.ps1
@@ -1,8 +1,5 @@
 start-process PowerShell -verb runas "C:\Users\Andrei\Github\dotfiles\Win\Startup\xauth.ps1"
 
-& komorebic start
-
 & "C:\Program Files\AutoHotkey\v2\AutoHotkey64.exe" "C:\Users\Andrei\Github\dotfiles\Win\AutoHotkey\ApplicationShortcuts.ahk"
 & "C:\Program Files\AutoHotkey\v2\AutoHotkey64.exe" "C:\Users\Andrei\Github\dotfiles\Win\AutoHotkey\Keymaps.ahk"
-& "C:\Program Files\AutoHotkey\v2\AutoHotkey64.exe" "C:\Users\Andrei\Github\dotfiles\Win\Komorebi\komorebic.ahk"
 & "C:\Program Files\AutoHotkey\v1.1.37.01\AutoHotkeyU64.exe" "C:\Users\Andrei\Github\dotfiles\Win\AutoHotkey\SwitchBetweenWindows.ahk"
