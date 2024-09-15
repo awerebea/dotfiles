74eed39e29e8a4ef0c29682636efa1eb45e71ed7
awerebea <awerebea.21@gmail.com>
2024-09-15 14:44:40 -0400

    NVIM Replace Copilot-related AI plugins with Codeium

--- nvim/.config/nvim/lua/plugins/extras/ai/codeium.lua
+++ nvim/.config/nvim/lua/plugins/extras/ai/codeium.lua
@@ -1,16 +1,18 @@
 return {
   {
     "Exafunction/codeium.vim",
-    enabled = false,
-    event = "InsertEnter",
+    enabled = true,
+    event = "BufEnter",
     -- stylua: ignore
     config = function ()
+      vim.g.codeium_manual = true
       vim.g.codeium_disable_bindings = 1
-      vim.keymap.set("i", "<M-l>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
-      vim.keymap.set("i", "<M-]>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
-      vim.keymap.set("i", "<M-[>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
-      vim.keymap.set("i", "<C-]>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
-      vim.keymap.set("i", "<M-a>", function() return vim.fn["codeium#Complete"]() end, { expr = true })
+      vim.keymap.set("i", "<M-;>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
+      vim.keymap.set("i", "<M-j>", function() return vim.fn["codeium#CycleOrComplete"]() end, { expr = true })
+      vim.keymap.set("i", "<M-k>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
+      vim.keymap.set("i", "<M-h>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
+      vim.keymap.set("i", "<M-w>", function() return vim.fn["codeium#AcceptNextWord"]() end, { expr = true })
+      vim.keymap.set("i", "<M-l>", function() return vim.fn["codeium#AcceptNextLine"]() end, { expr = true })
     end,
   },
 }
--- nvim/.config/nvim/lua/plugins/extras/ai/copilot-chat.lua
+++ nvim/.config/nvim/lua/plugins/extras/ai/copilot-chat.lua
@@ -1,5 +1,6 @@
 return {
   "CopilotC-Nvim/CopilotChat.nvim",
+  enabled = false,
   branch = "canary",
   dependencies = {
     { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
