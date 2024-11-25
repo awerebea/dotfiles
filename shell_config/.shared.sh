421e3e8decce5e27cc587650b78763a8fd718718
awerebea <awerebea.21@gmail.com>
2024-11-25 16:06:45 -0500

    GITCONFIG, BAT Fix Catppuccin-Mocha theme specification

--- shell_config/.shared.sh
+++ shell_config/.shared.sh
@@ -140,14 +140,15 @@ if command -v "bat" &>/dev/null; then
     if [[ ! -d "$bat_config/catppuccin" ]]; then
         mkdir -p "$bat_config"/themes
         git clone https://github.com/catppuccin/bat.git "$bat_config"/catppuccin
-        for file in "$bat_config"/catppuccin/Catppuccin-*.tmTheme; do
+        for file in "$bat_config"/catppuccin/themes/Catppuccin*.tmTheme; do
             if [ -e "$file" ]; then
-                ln -s "$file" "$bat_config"/themes/
+                theme_name="$(basename "$file")"
+                ln -s "$file" "$bat_config"/themes/"${theme_name// /-}"
             fi
         done
         bat cache --build
     fi
-    export BAT_THEME="Catppuccin-mocha"
+    export BAT_THEME="Catppuccin-Mocha"
 fi
 
 # Git helper functions
