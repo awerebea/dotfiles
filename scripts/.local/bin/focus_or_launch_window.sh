e33012c794c4ad8ec1454e62165bf4b37b77c6f3
awerebea <awerebea.21@gmail.com>
2024-11-27 23:33:10 -0500

    SCRIPTS Fix and improvements

--- scripts/.local/bin/focus_or_launch_window.sh
+++ scripts/.local/bin/focus_or_launch_window.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
 [[ $# -lt 1 ]] && exit 1
-if pgrep -x "$1" > /dev/null; then
+if pgrep -x "$1" >/dev/null; then
     xdotool windowactivate "$(xdotool search --classname "$1" | sort -h | head -n 1)"
 else
-    if [ "$1" == "chrome" ]; then
-        google-chrome
-    elif [ "$1" == "Telegram" ]; then
-        /home/andrei/.telegram/Telegram -workdir /home/andrei/.local/share/TelegramDesktop/ -- %u
+    if [ "$1" == "Telegram" ]; then
+        /home/andrei/.local/apps/Telegram/Telegram -workdir /home/andrei/.local/share/TelegramDesktop/ -- %u
     else
         "$1"
     fi
