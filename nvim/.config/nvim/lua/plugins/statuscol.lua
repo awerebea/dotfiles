1bbff4d41d0ee68a65296236cbe40833ec642872
awerebea <awerebea.21@gmail.com>
2024-12-10 12:27:41 -0500

    NVIM Update statuscol and foldcolumn settings

--- nvim/.config/nvim/lua/plugins/statuscol.lua
+++ nvim/.config/nvim/lua/plugins/statuscol.lua
@@ -2,16 +2,14 @@ return {
   "luukvbaal/statuscol.nvim",
   event = "VeryLazy",
   config = function()
-    if vim.fn.has "nvim-0.9" == 1 then
-      local builtin = require "statuscol.builtin"
-      require("statuscol").setup {
-        relculright = true,
-        segments = {
-          { text = { "%s" }, click = "v:lua.ScSa" },
-          { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
-          { text = { " ", builtin.foldfunc, " " }, click = "v:lua.ScFa" },
-        },
-      }
-    end
+    local builtin = require "statuscol.builtin"
+    require("statuscol").setup {
+      relculright = true,
+      segments = {
+        { text = { "%s" }, click = "v:lua.ScSa" },
+        { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
+        { text = { " ", builtin.foldfunc, " " }, click = "v:lua.ScFa" },
+      },
+    }
   end,
 }
