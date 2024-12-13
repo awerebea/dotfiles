eabaec775fec8a467c0cf877dad29e56cfb034e6
awerebea <awerebea.21@gmail.com>
2024-12-13 12:28:29 -0500

    NVIM Simplify nvim-scrollbar config

--- nvim/.config/nvim/lua/plugins/nvim-scrollbar.lua
+++ nvim/.config/nvim/lua/plugins/nvim-scrollbar.lua
@@ -2,7 +2,4 @@ return {
   "petertriho/nvim-scrollbar",
   event = "BufReadPre",
   opts = { marks = { GitChange = { text = "│" } } },
-  config = function(_, otps)
-    require("scrollbar").setup(otps)
-  end,
 }
