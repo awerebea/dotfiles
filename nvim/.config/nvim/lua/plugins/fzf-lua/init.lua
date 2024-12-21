99f38f9637c089ca56abe47aa4ece7e22146cf1a
awerebea <awerebea.21@gmail.com>
2024-12-21 12:08:11 -0500

    NVIM Update fzf-lua window opts

--- nvim/.config/nvim/lua/plugins/fzf-lua/init.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua/init.lua
@@ -20,9 +20,9 @@ return {
         -- "aboveleft vnew   : split left
         -- Only valid when using a float window
         -- (i.e. when 'split' is not defined, default)
-        height = 0.85, -- window height
-        width = 0.80, -- window width
-        row = 0.35, -- window row position (0=top, 1=bottom)
+        height = 0.95, -- window height
+        width = 1.0, -- window width
+        row = 0.80, -- window row position (0=top, 1=bottom)
         col = 0.50, -- window col position (0=left, 1=right)
         -- border argument passthrough to nvim_open_win(), also used
         -- to manually draw the border characters around the preview
@@ -52,9 +52,9 @@ return {
           wrap = "wrap", -- wrap|nowrap
           hidden = "nohidden", -- hidden|nohidden
           vertical = "down:45%", -- up|down:size
-          horizontal = "right:60%", -- right|left:size
+          horizontal = "right:40%", -- right|left:size
           layout = "flex", -- horizontal|vertical|flex
-          flip_columns = 100, -- #cols to switch to horizontal on flex
+          flip_columns = 220, -- #cols to switch to horizontal on flex
           -- Only used with the builtin previewer:
           title = true, -- preview border title (file/buf)?
           title_pos = "center", -- left|center|right, title alignment
