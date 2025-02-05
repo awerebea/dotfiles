009881303a2df6fd4eac9065445a312638355564
awerebea <awerebea.21@gmail.com>
2025-02-04 20:55:21 -0500

    chore: comment out xauth.ps1 startup script
    Commented out the xauth.ps1 script in the startup.ps1 file to prevent
    it from running at startup. Use insecure VcXsrv connection instead.

--- Win/Startup/startup.ps1
+++ Win/Startup/startup.ps1
@@ -1,4 +1,4 @@
-start-process PowerShell -verb runas "C:\Users\Andrei\Github\dotfiles\Win\Startup\xauth.ps1"
+# start-process PowerShell -verb runas "C:\Users\Andrei\Github\dotfiles\Win\Startup\xauth.ps1"
 
 & "C:\Program Files\AutoHotkey\v2\AutoHotkey64.exe" "C:\Users\Andrei\Github\dotfiles\Win\AutoHotkey\ApplicationShortcuts.ahk"
 & "C:\Program Files\AutoHotkey\v2\AutoHotkey64.exe" "C:\Users\Andrei\Github\dotfiles\Win\AutoHotkey\Keymaps.ahk"
