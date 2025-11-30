ef8b32f5423bd9ce78b1923478be555172b3f8d8
awerebea <awerebea.21@gmail.com>
2025-11-29 23:26:05 -0500

    feat(folding): add nvim-origami plugin config
    Add configuration for the nvim-origami plugin to enable automatic
    folding of comments and imports. Set up custom fold keymaps and disable
    Vim's default auto-folding by adjusting foldlevel options. This improves
    code navigation and folding behavior in Neovim.

---
+++ nvim/.config/nvim/lua/plugins/nvim-origami.lua
@@ -0,0 +1,20 @@
+return {
+  "chrisgrieser/nvim-origami",
+  event = "VeryLazy",
+  opts = {
+    autoFold = {
+      enabled = true,
+      kinds = { "comment", "imports" }, ---@type lsp.FoldingRangeKind[]
+    },
+    foldKeymaps = {
+      setup = true, -- modifies `h`, `l`, and `$`
+      hOnlyOpensOnFirstColumn = false,
+    },
+  }, -- needed even when using default config
+
+  -- recommended: disable vim's auto-folding
+  init = function()
+    vim.opt.foldlevel = 99
+    vim.opt.foldlevelstart = 99
+  end,
+}
