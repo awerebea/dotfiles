05f9651eec9d397f76726564f4b699c335537eda
awerebea <awerebea.21@gmail.com>
2026-03-19 13:38:41 -0400

    feat: update nvim-surround visual keymap
    - Enable the nvim-surround plugin explicitly
    - Remove the default 'gS' visual mode mapping
    - Add a new visual mode mapping for '<leader>gS' to surround visual
      selections on new lines

--- nvim/.config/nvim/lua/plugins/nvim-surround.lua
+++ nvim/.config/nvim/lua/plugins/nvim-surround.lua
@@ -1,9 +1,11 @@
 return {
   "kylechui/nvim-surround",
+  enabled = true,
   version = "*", -- Use for stability; omit to use `main` branch for the latest features
   event = "VeryLazy",
   config = function()
-    vim.keymap.set("x", "<leader>gS", "<Plug>(nvim-surround-visual-line)", {
+    vim.keymap.del("v", "gS")
+    vim.keymap.set("v", "<leader>gS", "<Plug>(nvim-surround-visual-line)", {
       desc = "Add a surrounding pair around a visual selection, on new lines",
     })
   end,
