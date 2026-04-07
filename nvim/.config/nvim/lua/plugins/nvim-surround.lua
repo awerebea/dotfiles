34c4a5d2cadf88c19e7b7597c32e5179c661c848
awerebea <awerebea.21@gmail.com>
2026-04-07 14:42:54 -0400

    refactor: update nvim-surround visual mappings
    - Set `vim.g.nvim_surround_no_visual_mappings` before plugin loads to
      prevent default gS visual mapping registration.
    - Restore visual surround to "S" in visual mode.
    - Move visual-line surround to "<leader>gS" to avoid conflict with
      mini.splitjoin.
    - Update keymap descriptions for clarity.

--- nvim/.config/nvim/lua/plugins/nvim-surround.lua
+++ nvim/.config/nvim/lua/plugins/nvim-surround.lua
@@ -3,9 +3,17 @@ return {
   enabled = true,
   version = "*", -- Use for stability; omit to use `main` branch for the latest features
   event = "VeryLazy",
+  init = function()
+    -- Must be set before the plugin loads so it doesn't register the default gS visual mapping
+    vim.g.nvim_surround_no_visual_mappings = true
+  end,
   config = function()
-    vim.keymap.del("v", "gS")
-    vim.keymap.set("v", "<leader>gS", "<Plug>(nvim-surround-visual-line)", {
+    require("nvim-surround").setup()
+    -- Restore visual surround on S; move visual-line off gS so mini.splitjoin owns it
+    vim.keymap.set("x", "S", "<Plug>(nvim-surround-visual)", {
+      desc = "Add a surrounding pair around a visual selection",
+    })
+    vim.keymap.set("x", "<leader>gS", "<Plug>(nvim-surround-visual-line)", {
       desc = "Add a surrounding pair around a visual selection, on new lines",
     })
   end,
