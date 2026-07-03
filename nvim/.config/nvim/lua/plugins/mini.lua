baf420e476e01ca539a587ba40fc93c713fb060a
awerebea <awerebea.21@gmail.com>
2026-07-03 00:29:57 -0400

    chore(nvim): remove commented-out mini plugins
    Remove dead commented-out plugin configurations for mini.comment
    and mini.pairs from the mini.lua plugin spec.

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -78,25 +78,4 @@ return {
     version = "*",
     config = true,
   },
-  -- {
-  --   "echasnovski/mini.comment",
-  --   event = "VeryLazy",
-  --   opts = {
-  --     hooks = {
-  --       pre = function()
-  --         require("ts_context_commentstring.internal").update_commentstring {}
-  --       end,
-  --     },
-  --   },
-  --   config = function(_, opts)
-  --     require("mini.comment").setup(opts)
-  --   end,
-  -- },
-  -- {
-  --   "echasnovski/mini.pairs",
-  --   event = "VeryLazy",
-  --   config = function(_, opts)
-  --     require("mini.pairs").setup(opts)
-  --   end,
-  -- },
 }
