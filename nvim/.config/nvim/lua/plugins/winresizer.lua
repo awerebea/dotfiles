f260f581f0d4559bd5349e7d6cba5e027904d648
awerebea <awerebea.21@gmail.com>
2026-07-03 00:44:57 -0400

    refactor(winresizer): remove unused config params
    Remove unused `_` parameters from the config function
    signature, as neither the plugin object nor options are
    referenced in the body.

--- nvim/.config/nvim/lua/plugins/winresizer.lua
+++ nvim/.config/nvim/lua/plugins/winresizer.lua
@@ -2,7 +2,7 @@ return {
   "simeji/winresizer",
   cmd = "WinResizerStartResize",
   keys = { "<C-e>" },
-  config = function(_, _)
+  config = function()
     vim.g.winresizer_finish_with_escape = 1
     vim.g.winresizer_vert_resize = 3
     vim.g.winresizer_horiz_resize = 3
