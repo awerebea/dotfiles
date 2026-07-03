f8e9b5d9f3b14aa1ee2d99c44c31779f53a169d3
awerebea <awerebea.21@gmail.com>
2026-07-03 00:43:08 -0400

    chore(trim): enable blank line collapsing pattern
    Uncomment the substitution pattern that replaces multiple
    consecutive blank lines with a single blank line.

--- nvim/.config/nvim/lua/plugins/trim.lua
+++ nvim/.config/nvim/lua/plugins/trim.lua
@@ -6,7 +6,7 @@ return {
       -- "markdown",
     },
     patterns = {
-      -- [[%s/\(\n\n\)\n\+/\1/]], -- replace multiple blank lines with a single line
+      [[%s/\(\n\n\)\n\+/\1/]], -- replace multiple blank lines with a single line
     },
     trim_on_write = false,
     trim_trailing = true,
