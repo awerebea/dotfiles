0c6026e160e643294e3fc713708d8084a53cb29b
awerebea <awerebea.21@gmail.com>
2026-07-10 00:49:28 -0400

    chore(nvim): remove unused and disabled plugins
    Remove several plugins that were disabled (enabled = false) or no
    longer needed:
    
    - Remove nvim-dap and related lua debugger configuration
    - Remove ChatGPT, Codeium AI plugins
    - Remove Python and Rust DAP configurations
    - Remove leap.nvim and flit.nvim motion plugins
    - Remove barbecue.nvim from LSP config
    - Remove lua-format, neo-tree, neowords, nerdcommenter
    - Remove nvim-colorizer, nvim-origami, nvim-ufo
    - Remove worktrees.nvim, YankAssassin, yanky.nvim

--- nvim/.config/nvim/lua/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/plugins/lsp/init.lua
@@ -151,17 +151,6 @@ return {
     end,
   },
   {
-    "utilyre/barbecue.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "neovim/nvim-lspconfig",
-      "SmiteshP/nvim-navic",
-      "nvim-tree/nvim-web-devicons",
-    },
-    enabled = false, -- use dropbar instead
-    config = true,
-  },
-  {
     "ErichDonGubler/lsp_lines.nvim",
     event = "VeryLazy",
     config = function()
--- nvim/.config/nvim/lua/plugins/yank-assassin.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "svban/YankAssassin.nvim",
-  enabled = false,
-  event = "VeryLazy",
-  opts = {
-    auto_normal = true,
-    auto_visual = true,
-  },
-}
